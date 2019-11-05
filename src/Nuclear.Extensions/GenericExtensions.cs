cbb0c10615d027f3d440e115dfdcc454d95f77a0
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2019-11-05 20:04:36 +0100

    add extensions for IEnumerable and more

---
+++ src/Nuclear.Extensions/GenericExtensions.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Nuclear.Extensions {
+
+    /// <summary>
+    /// Provides extension methods for any type.
+    /// </summary>
+    public static class GenericExtensions {
+
+        /// <summary>
+        /// Gets a <see cref="String"/> representing <paramref name="_this"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of <paramref name="_this"/>.</typeparam>
+        /// <param name="_this">The object in question.</param>
+        /// <returns>The formatted <see cref="String"/>.</returns>
+        /// <example>
+        /// <code>
+        /// Console.WriteLine(someObject.Format());
+        /// </code>
+        /// </example>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static String Format<T>(this T _this) {
+            if(_this == null) { return "'null'"; }
+
+            if(_this is Type type) { return String.Format(CultureInfo.InvariantCulture, "'{0}'", type.FullName); }
+
+            return String.Format(CultureInfo.InvariantCulture, "'{0}'", _this);
+        }
+
+        /// <summary>
+        /// Gets a <see cref="String"/> representing the type of <paramref name="_this"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of <paramref name="_this"/>.</typeparam>
+        /// <param name="_this">The object in question.</param>
+        /// <returns>The formatted <see cref="String"/>.</returns>
+        /// <example>
+        /// <code>
+        /// Console.WriteLine(someObject.FormatType());
+        /// </code>
+        /// </example>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static String FormatType<T>(this T _this) => _this != null ? _this.GetType().Format() : _this.Format();
+
+        /// <summary>
+        /// Determines equality of <paramref name="left"/> and <paramref name="right"/> using the implementations of
+        ///     <see cref="IEquatable{T}"/>, <see cref="IComparable{T}"/>, <see cref="IComparable"/> and the default <see cref="IEqualityComparer{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the objects to compare.</typeparam>
+        /// <param name="left">The left object.</param>
+        /// <param name="right">The right object.</param>
+        /// <returns>True if both objects are equal or null.</returns>
+        /// <example>
+        /// <code>
+        /// if(someObject.IsEqual(someOtherObject)) {
+        ///     // ...
+        /// }
+        /// </code>
+        /// </example>
+        public static Boolean IsEqual<T>(this T left, T right) {
+
+            if(left == null) {
+                if(right == null) {
+                    return true;
+                } else {
+                    return right.IsEqual(left);
+                }
+            }
+
+            if(left is IEquatable<T> eLeft) {
+                try {
+                    return eLeft.Equals(right);
+                } catch { /* advance to next */ }
+            }
+
+            if(left is IComparable<T> cTLeft) {
+                try {
+                    return cTLeft.CompareTo(right) == 0;
+                } catch { /* advance to next */ }
+            }
+
+            if(left is IComparable cLeft) {
+                try {
+                    return cLeft.CompareTo(right) == 0;
+                } catch { /* advance to next */ }
+            }
+
+            return IsEqual(left, right, EqualityComparer<T>.Default);
+        }
+
+        /// <summary>
+        /// Determines equality of <paramref name="left"/> and <paramref name="right"/> using an <see cref="IEqualityComparer{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the objects to compare.</typeparam>
+        /// <param name="left">The left object.</param>
+        /// <param name="right">The right object.</param>
+        /// <param name="comparer">The comparer used to establish equality. Fallback is the default <see cref="IEqualityComparer{T}"/>.</param>
+        /// <returns>True if both objects are equal or null.</returns>
+        /// <example>
+        /// <code>
+        /// if(someObject.IsEqual(someOtherObject, new SomeClassEqualityComparer())) {
+        ///     // ...
+        /// }
+        /// </code>
+        /// </example>
+        public static Boolean IsEqual<T>(this T left, T right, IEqualityComparer<T> comparer) {
+            if(comparer == null) {
+                comparer = EqualityComparer<T>.Default;
+            }
+
+            try {
+                return comparer.Equals(left, right);
+
+            } catch { }
+
+            return false;
+        }
+
+    }
+}
---
+++ src/Nuclear.Extensions/IEnumerableTExtensions.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nuclear.Exceptions;
+
+namespace Nuclear.Extensions {
+
+    /// <summary>
+    /// The class <see cref="IEnumerableTExtensions"/> provides extension methods to the type <see cref="IEnumerable{T}"/>.
+    /// These methods add either completely new functionality or enhanced functionality based on existing implementations.
+    /// </summary>
+    public static class IEnumerableTExtensions {
+
+        /// <summary>
+        /// Invokes an <paramref name="action"/> on every item in <paramref name="_this"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of items in <paramref name="_this"/>.</typeparam>
+        /// <param name="_this">The <see cref="IEnumerable{T}"/> that is used.</param>
+        /// <param name="action">The <see cref="Action{T}"/> to invoke.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="_this"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="action"/> is null.</exception>
+        /// <example>
+        /// <code>
+        /// myEnumeration.ForEach(element => doSomething(element));
+        /// </code>
+        /// </example>
+        public static void ForEach<T>(this IEnumerable<T> _this, Action<T> action) {
+            Throw.If.Null(_this, "_this");
+            Throw.If.Null(action, "action");
+
+            foreach(T element in _this) {
+                action(element);
+            }
+        }
+
+        /// <summary>
+        /// Gets the minimum value in <paramref name="_this"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of items in <paramref name="_this"/> implements <see cref="IComparable"/>.</typeparam>
+        /// <param name="_this">The <see cref="IEnumerable{T}"/> that is used.</param>
+        /// <returns>The minimum value in <paramref name="_this"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="_this"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="_this"/> is empty.</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="_this"/> contains null values only.</exception>
+        /// <example>
+        /// <code>
+        /// var min = myEnumeration.Min();
+        /// </code>
+        /// </example>
+        public static T Min<T>(this IEnumerable<T> _this) where T : IComparable {
+            Throw.If.Null(_this, "_this");
+            Throw.If.False(_this.Count() > 0, "_this", "The enumeration is empty.");
+            Throw.If.False(_this.Any((element) => element != null), "_this", "The enumeration only contains null values.");
+
+            T min = default;
+            Action<T> action = (element) => {
+                if(min == null) {
+                    min = element;
+                }
+
+                if(element != null && min != null && element.CompareTo(min) < 0) {
+                    min = element;
+                }
+            };
+
+            _this.ForEach(action);
+
+            return min;
+        }
+
+        /// <summary>
+        /// Gets the minimum value in <paramref name="_this"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of items in <paramref name="_this"/> implements <see cref="IComparable{T}"/>.</typeparam>
+        /// <param name="_this">The <see cref="IEnumerable{T}"/> that is used.</param>
+        /// <returns>The minimum value in <paramref name="_this"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="_this"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="_this"/> is empty.</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="_this"/> contains null values only.</exception>
+        /// <example>
+        /// <code>
+        /// var min = myEnumeration.MinT();
+        /// </code>
+        /// </example>
+        public static T MinT<T>(this IEnumerable<T> _this) where T : IComparable<T> {
+            Throw.If.Null(_this, "_this");
+            Throw.If.False(_this.Count() > 0, "_this", "The enumeration is empty.");
+            Throw.If.False(_this.Any((element) => element != null), "_this", "The enumeration only contains null values.");
+
+            T min = default;
+            Action<T> action = (element) => {
+                if(min == null) {
+                    min = element;
+                }
+
+                if(element != null && min != null && element.CompareTo(min) < 0) {
+                    min = element;
+                }
+            };
+
+            _this.ForEach(action);
+
+            return min;
+        }
+
+        /// <summary>
+        /// Gets the maximum value in <paramref name="_this"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of items in <paramref name="_this"/> implements <see cref="IComparable"/>.</typeparam>
+        /// <param name="_this">The <see cref="IEnumerable{T}"/> that is used.</param>
+        /// <returns>The maximum value in <paramref name="_this"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="_this"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="_this"/> is empty.</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="_this"/> contains null values only.</exception>
+        /// <example>
+        /// <code>
+        /// var min = myEnumeration.Max();
+        /// </code>
+        /// </example>
+        public static T Max<T>(this IEnumerable<T> _this) where T : IComparable {
+            Throw.If.Null(_this, "_this");
+            Throw.If.False(_this.Count() > 0, "_this", "The enumeration is empty.");
+            Throw.If.False(_this.Any((element) => element != null), "_this", "The enumeration only contains null values.");
+
+            T max = default;
+            Action<T> action = (element) => {
+                if(max == null) {
+                    max = element;
+                }
+
+                if(element != null && max != null && element.CompareTo(max) > 0) {
+                    max = element;
+                }
+            };
+
+            _this.ForEach(action);
+
+            return max;
+        }
+
+        /// <summary>
+        /// Gets the maximum value in <paramref name="_this"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of items in <paramref name="_this"/> implements <see cref="IComparable{T}"/>.</typeparam>
+        /// <param name="_this">The <see cref="IEnumerable{T}"/> that is used.</param>
+        /// <returns>The maximum value in <paramref name="_this"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="_this"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="_this"/> is empty.</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="_this"/> contains null values only.</exception>
+        /// <example>
+        /// <code>
+        /// var min = myEnumeration.MaxT();
+        /// </code>
+        /// </example>
+        public static T MaxT<T>(this IEnumerable<T> _this) where T : IComparable<T> {
+            Throw.If.Null(_this, "_this");
+            Throw.If.False(_this.Count() > 0, "_this", "The enumeration is empty.");
+            Throw.If.False(_this.Any((element) => element != null), "_this", "The enumeration only contains null values.");
+
+            T max = default;
+            Action<T> action = (element) => {
+                if(max == null) {
+                    max = element;
+                }
+
+                if(element != null && max != null && element.CompareTo(max) > 0) {
+                    max = element;
+                }
+            };
+
+            _this.ForEach(action);
+
+            return max;
+        }
+
+    }
+}

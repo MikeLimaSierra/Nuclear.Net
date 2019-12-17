db10fac294b9aecd1388ed72066819b8021986d6
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2019-12-17 17:48:36 +0100

    split exception tests

---
+++ src/Nuclear.Exceptions.Tests/ObjectExceptionSuite_uTests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Nuclear.TestSite;
+
+namespace Nuclear.Exceptions {
+
+    class ObjectExceptionSuite_uTests {
+
+        private static readonly String _paramName = "fake_param_name";
+
+        private static readonly String _message = "fake_exception_message";
+
+        #region Null
+
+        [TestMethod]
+        void ThrowIfNull() {
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.Object.IsNull(null, _paramName, _message), out ArgumentNullException ex1);
+            Test.If.Value.Equals(_paramName, ex1.ParamName);
+            Test.If.String.StartsWith(ex1.Message, _message);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.If.Object.IsNull(new Object(), _paramName, _message), out Exception ex2);
+
+        }
+
+        [TestMethod]
+        void ThrowIfNotNull() {
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.IfNot.Object.IsNull(null, _paramName, _message), out Exception ex1);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.IfNot.Object.IsNull(new Object(), _paramName, _message), out ArgumentNullException ex2);
+            Test.If.Value.Equals(_paramName, ex2.ParamName);
+            Test.If.String.StartsWith(ex2.Message, _message);
+
+        }
+
+        [TestMethod]
+        void ThrowIfNullGeneric() {
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.Object.IsNull<NotImplementedException>(null, _message), out NotImplementedException ex1);
+            Test.If.String.StartsWith(ex1.Message, _message);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.If.Object.IsNull<NotImplementedException>(new Object(), _message), out Exception ex2);
+
+        }
+
+        [TestMethod]
+        void ThrowIfNotNullGeneric() {
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.IfNot.Object.IsNull<NotImplementedException>(null, _message), out Exception ex1);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.IfNot.Object.IsNull<NotImplementedException>(new Object(), _message), out NotImplementedException ex2);
+            Test.If.String.StartsWith(ex2.Message, _message);
+
+        }
+
+        #endregion
+
+        #region OfType
+
+        [TestMethod]
+        void ThrowIfOfType() {
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.Object.IsOfType<String>("STRING", _paramName, _message), out ArgumentException ex1);
+            Test.If.Value.Equals(_paramName, ex1.ParamName);
+            Test.If.String.StartsWith(ex1.Message, _message);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.If.Object.IsOfType<String>(42, _paramName, _message), out Exception ex2);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.Object.IsOfType<String>(null, _paramName, _message), out ArgumentNullException ex3);
+
+        }
+
+        [TestMethod]
+        void ThrowIfNotOfType() {
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.IfNot.Object.IsOfType<String>("STRING", _paramName, _message), out Exception ex1);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.IfNot.Object.IsOfType<String>(42, _paramName, _message), out ArgumentException ex2);
+            Test.If.Value.Equals(_paramName, ex2.ParamName);
+            Test.If.String.StartsWith(ex2.Message, _message);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.IfNot.Object.IsOfType<String>(null, _paramName, _message), out ArgumentNullException ex3);
+
+        }
+
+        [TestMethod]
+        void ThrowIfOfTypeGeneric() {
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.Object.IsOfType<NotImplementedException, String>("STRING", _message), out NotImplementedException ex1);
+            Test.If.String.StartsWith(ex1.Message, _message);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.If.Object.IsOfType<NotImplementedException, String>(42, _message), out Exception ex2);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.Object.IsOfType<NotImplementedException, String>(null, _message), out ArgumentNullException ex3);
+
+        }
+
+        [TestMethod]
+        void ThrowIfNotOfTypeGeneric() {
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.IfNot.Object.IsOfType<NotImplementedException, String>("STRING", _message), out Exception ex1);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.IfNot.Object.IsOfType<NotImplementedException, String>(42, _message), out NotImplementedException ex2);
+            Test.If.String.StartsWith(ex2.Message, _message);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.IfNot.Object.IsOfType<NotImplementedException, String>(null, _message), out ArgumentNullException ex3);
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/Nuclear.Exceptions.Tests/StringExceptionSuite_uTests.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using Nuclear.TestSite;
+
+namespace Nuclear.Exceptions {
+
+    class StringExceptionSuite_uTests {
+
+        private static readonly String _paramName = "fake_param_name";
+
+        private static readonly String _message = "fake_exception_message";
+
+        #region NullOrEmpty
+
+        [TestMethod]
+        void ThrowIfNullOrEmpty() {
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrEmpty(null, _paramName, _message), out ArgumentNullException ex1);
+            Test.If.Value.Equals(_paramName, ex1.ParamName);
+            Test.If.String.StartsWith(ex1.Message, _message);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrEmpty(String.Empty, _paramName, _message), out ArgumentException ex2);
+            Test.If.Value.Equals(_paramName, ex2.ParamName);
+            Test.If.String.StartsWith(ex2.Message, _message);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrEmpty(" ", _paramName, _message), out Exception ex3);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrEmpty("STRING", _paramName, _message), out Exception ex4);
+
+        }
+
+        [TestMethod]
+        void ThrowIfNotNullOrEmpty() {
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrEmpty(null, _paramName, _message), out Exception ex1);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrEmpty(String.Empty, _paramName, _message), out Exception ex2);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrEmpty(" ", _paramName, _message), out ArgumentException ex3);
+            Test.If.Value.Equals(_paramName, ex3.ParamName);
+            Test.If.String.StartsWith(ex3.Message, _message);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrEmpty("STRING", _paramName, _message), out ArgumentException ex4);
+            Test.If.Value.Equals(_paramName, ex4.ParamName);
+            Test.If.String.StartsWith(ex4.Message, _message);
+
+        }
+
+        [TestMethod]
+        void ThrowIfNullOrEmptyGeneric() {
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrEmpty<NotImplementedException>(null, _message), out NotImplementedException ex1);
+            Test.If.String.StartsWith(ex1.Message, _message);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrEmpty<NotImplementedException>(String.Empty, _message), out NotImplementedException ex2);
+            Test.If.String.StartsWith(ex2.Message, _message);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrEmpty<NotImplementedException>(" ", _message), out Exception ex3);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrEmpty<NotImplementedException>("STRING", _message), out Exception ex4);
+
+        }
+
+        [TestMethod]
+        void ThrowIfNotNullOrEmptyGeneric() {
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrEmpty<NotImplementedException>(null, _message), out Exception ex1);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrEmpty<NotImplementedException>(String.Empty, _message), out Exception ex2);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrEmpty<NotImplementedException>(" ", _message), out NotImplementedException ex3);
+            Test.If.String.StartsWith(ex3.Message, _message);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrEmpty<NotImplementedException>("STRING", _message), out NotImplementedException ex4);
+            Test.If.String.StartsWith(ex4.Message, _message);
+
+        }
+
+        #endregion
+
+        #region NullOrWhiteSpace
+
+        [TestMethod]
+        void ThrowIfNullOrWhiteSpace() {
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrWhiteSpace(null, _paramName, _message), out ArgumentNullException ex1);
+            Test.If.Value.Equals(_paramName, ex1.ParamName);
+            Test.If.String.StartsWith(ex1.Message, _message);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrWhiteSpace(String.Empty, _paramName, _message), out ArgumentException ex2);
+            Test.If.Value.Equals(_paramName, ex2.ParamName);
+            Test.If.String.StartsWith(ex2.Message, _message);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrWhiteSpace(" ", _paramName, _message), out ArgumentException ex3);
+            Test.If.Value.Equals(_paramName, ex3.ParamName);
+            Test.If.String.StartsWith(ex3.Message, _message);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrWhiteSpace("STRING", _paramName, _message), out Exception ex4);
+
+        }
+
+        [TestMethod]
+        void ThrowIfNotNullOrWhiteSpace() {
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrWhiteSpace(null, _paramName, _message), out Exception ex1);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrWhiteSpace(String.Empty, _paramName, _message), out Exception ex2);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrWhiteSpace(" ", _paramName, _message), out Exception ex3);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrWhiteSpace("STRING", _paramName, _message), out ArgumentException ex4);
+            Test.If.Value.Equals(_paramName, ex4.ParamName);
+            Test.If.String.StartsWith(ex4.Message, _message);
+
+        }
+
+        [TestMethod]
+        void ThrowIfNullOrWhiteSpaceGeneric() {
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrWhiteSpace<NotImplementedException>(null, _message), out NotImplementedException ex1);
+            Test.If.String.StartsWith(ex1.Message, _message);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrWhiteSpace<NotImplementedException>(String.Empty, _message), out NotImplementedException ex2);
+            Test.If.String.StartsWith(ex2.Message, _message);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrWhiteSpace<NotImplementedException>(" ", _message), out NotImplementedException ex3);
+            Test.If.String.StartsWith(ex3.Message, _message);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.If.String.IsNullOrWhiteSpace<NotImplementedException>("STRING", _message), out Exception ex4);
+
+        }
+
+        [TestMethod]
+        void ThrowIfNotNullOrWhiteSpaceGeneric() {
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrWhiteSpace<NotImplementedException>(null, _message), out Exception ex1);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrWhiteSpace<NotImplementedException>(String.Empty, _message), out Exception ex2);
+
+            Test.IfNot.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrWhiteSpace<NotImplementedException>(" ", _message), out Exception ex3);
+
+            Test.If.Action.ThrowsException(() =>
+                Throw.IfNot.String.IsNullOrWhiteSpace<NotImplementedException>("STRING", _message), out NotImplementedException ex4);
+            Test.If.String.StartsWith(ex4.Message, _message);
+
+        }
+
+        #endregion
+
+    }
+}

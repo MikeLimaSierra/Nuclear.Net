8830b056680115255edca4db523625fc15f5bc52
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-01-31 23:11:20 +0100

    type formatting overhauled

--- src/Nuclear.Extensions/TypeExtensions.cs
+++ src/Nuclear.Extensions/TypeExtensions.cs
@@ -5,14 +5,29 @@ using System.Text;
 using Nuclear.Exceptions;
 
 namespace Nuclear.Extensions {
-public static    class TypeExtensions {
 
+    /// <summary>
+    /// The class <see cref="TypeExtensions"/> provides extension methods to the type <see cref="Type"/>.
+    /// These methods add either completely new functionality or enhanced functionality based on existing implementations.
+    /// </summary>
+    public static class TypeExtensions {
+
+        /// <summary>
+        /// Resolves the friendly name to a given <see cref="Type"/>.
+        /// </summary>
+        /// <param name="_this">The <see cref="Type"/> to resolve.</param>
+        /// <returns>The friendly name.</returns>
         public static String ResolveFriendlyName(this Type _this) {
             Throw.If.Object.IsNull(_this, nameof(_this));
 
-            String name = $"{_this.Namespace}.{_this.Name}";
+            String name = _this.FullName;
 
-            if(name.Contains("`")) { name = name.Remove(name.IndexOf('`')); }
+            if(_this.IsArray && name.EndsWith("[]")) {
+                name = $"{Type.GetType(name.Substring(0, name.Length - 2)).ResolveFriendlyName()}[]";
+            } else if(name.Contains("`")) {
+                name = name.Remove(name.IndexOf('`'));
+            }
+            
 
             if(_this.GenericTypeArguments.Length > 0) {
                 name += $"<{String.Join(", ", _this.GenericTypeArguments.Select(type => type.ResolveFriendlyName()))}>";

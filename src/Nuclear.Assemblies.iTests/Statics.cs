26ac286af49771bb4d165d3d0ca6f25ccd7a2b82
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-02-04 18:07:17 +0100

    add static testrelated objects

---
+++ src/Nuclear.Assemblies.iTests/Statics.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Nuclear.Assemblies.Resolvers;
+using Nuclear.Extensions;
+
+namespace Nuclear.Assemblies {
+    internal static class Statics {
+
+        internal static IEqualityComparer<FileInfo> FileInfoComparer { get; } = DynamicEqualityComparer.FromDelegate<FileInfo>(
+            (x, y) => (x == null && y == null) || (x != null && y != null && x.FullName.Equals(y.FullName)),
+            obj => obj.GetHashCode());
+
+        internal static Assembly EntryAsm { get; } = Assembly.GetEntryAssembly();
+
+        internal static FileInfo EntryPath { get; } = new FileInfo(EntryAsm.Location);
+
+        internal static Assembly TestAsm { get; } = typeof(DefaultResolver_iTests).Assembly;
+
+        internal static FileInfo TestPath { get; } = new FileInfo(TestAsm.Location);
+    }
+}

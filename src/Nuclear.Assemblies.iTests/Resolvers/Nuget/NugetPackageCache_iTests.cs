753a3dd00f7c318f887498b177808ad507ab8f7c
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-02-25 19:15:52 +0100

    nuget package cache

---
+++ src/Nuclear.Assemblies.iTests/Resolvers/Nuget/NugetPackageCache_iTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+using Nuclear.Extensions;
+using Nuclear.TestSite;
+
+using static System.Environment;
+
+namespace Nuclear.Assemblies.Resolvers.Nuget {
+    class NugetPackageCache_iTests {
+
+        #region GetCaches
+
+        [TestMethod]
+        void GetCaches() {
+
+            IEnumerable<NugetPackageCache> caches = null;
+
+            String userPath = Path.Combine(GetFolderPath(SpecialFolder.UserProfile), ".nuget", "packages");
+
+            Test.IfNot.Action.ThrowsException(() => caches = NugetPackageCache.GetCaches(), out Exception ex);
+
+            Test.If.Value.IsEqual(caches.Count(), 1);
+            Test.If.Value.IsEqual(caches.First().Location.FullName, userPath);
+
+        }
+
+        #endregion
+
+        #region HasPackage
+
+        [TestMethod]
+        void HasPackage() {
+
+            NugetPackageCache cache = NugetPackageCache.GetCaches().First();
+
+            Test.If.Action.ThrowsException(() => cache.HasPackage(null), out ArgumentNullException anex);
+            Test.If.Action.ThrowsException(() => cache.HasPackage(""), out ArgumentException aex);
+            Test.If.Action.ThrowsException(() => cache.HasPackage(" "), out aex);
+
+            DDTHasPackage("microsoft.csharp", true);
+            DDTHasPackage("netstandard.library", true);
+            DDTHasPackage("non.existent.library", false);
+
+        }
+
+        void DDTHasPackage(String input, Boolean expected,
+            [CallerFilePath] String _file = null, [CallerMemberName] String _method = null) {
+
+            NugetPackageCache cache = NugetPackageCache.GetCaches().First();
+
+            Boolean result = false;
+
+            Test.Note($"NugetPackageCache.HasPackage({input.Format()}) == {expected.Format()}", _file, _method);
+
+            Test.IfNot.Action.ThrowsException(() => result = cache.HasPackage(input), out Exception ex, _file, _method);
+
+            Test.If.Value.IsEqual(result, expected, _file, _method);
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/Nuclear.Assemblies/Resolvers/Nuget/NugetPackageCache.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Nuclear.Exceptions;
+using Nuclear.Extensions;
+
+namespace Nuclear.Assemblies.Resolvers.Nuget {
+    internal class NugetPackageCache {
+
+        #region fields
+
+        private static readonly String _nugetDirName = ".nuget";
+
+        private static readonly String _packagesDirName = "packages";
+
+        #endregion
+
+        #region properties
+
+        internal DirectoryInfo Location { get; private set; }
+
+        #endregion
+
+        #region ctors
+
+        internal NugetPackageCache(DirectoryInfo location) {
+            Throw.If.Object.IsNull(location, nameof(location));
+            Throw.If.Value.IsFalse(location.Exists, nameof(location), $"{location.FullName.Format()} doesn't exist!");
+
+            Location = location;
+        }
+
+        #endregion
+
+        #region static methods
+
+        internal static IEnumerable<NugetPackageCache> GetCaches() {
+            List<NugetPackageCache> caches = new List<NugetPackageCache>();
+
+            DirectoryInfo userProfileDir = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+            DirectoryInfo usersDir = userProfileDir.Parent;
+
+            DirectoryInfo userNugetDir = new DirectoryInfo(Path.Combine(userProfileDir.FullName, _nugetDirName, _packagesDirName));
+
+            if(userNugetDir.Exists) {
+                caches.Add(new NugetPackageCache(userNugetDir));
+            }
+
+            foreach(DirectoryInfo userDir in usersDir.EnumerateDirectories()) {
+                try {
+                    DirectoryInfo packageDir = new DirectoryInfo(Path.Combine(userDir.FullName, _nugetDirName, _packagesDirName));
+
+                    if(packageDir.FullName != userNugetDir.FullName && packageDir.Exists) {
+                        caches.Add(new NugetPackageCache(packageDir));
+                    }
+
+                } catch { /* Don't care if we can't access drectories */ }
+
+            }
+
+            return caches;
+        }
+
+        #endregion
+
+        #region methods
+
+        internal Boolean HasPackage(String name) {
+            Throw.If.String.IsNullOrWhiteSpace(name, nameof(name));
+
+            String lowerCaseName = name.ToLower();
+
+            return Location.EnumerateDirectories().Any((dir) => dir.Name.ToLower() == lowerCaseName);
+        }
+
+        #endregion
+
+    }
+}

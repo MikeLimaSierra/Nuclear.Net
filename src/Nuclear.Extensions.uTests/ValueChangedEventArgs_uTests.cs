9f32484ac3bae2515b0118bee4e6b6735d05ebc3
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-04-29 22:21:52 +0200

    Update ValueChangedEventArgs_uTests.cs

--- src/Nuclear.Extensions.uTests/ValueChangedEventArgs_uTests.cs
+++ src/Nuclear.Extensions.uTests/ValueChangedEventArgs_uTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Nuclear.TestSite;
 
@@ -6,41 +7,22 @@ namespace Nuclear.Extensions {
     class ValueChangedEventArgs_uTests {
 
         [TestMethod]
-        void ConstructorNullable() {
-
-            String _old = "old";
-            String _new = "new";
-
-            DDTConstructor<String>(null, null, false);
-            DDTConstructor(null, _new, true);
-            DDTConstructor(_old, null, true);
-            DDTConstructor(_old, _new, true);
-            DDTConstructor(_old, _old, false);
-
-        }
-
-        [TestMethod]
-        void ConstructorNonNullable() {
-
-            Int32 _old = 5;
-            Int32 _new = 6;
-
-            DDTConstructor(_old, _new, true);
-            DDTConstructor(_old, _old, false);
-
-        }
-
-        void DDTConstructor<TValue>(TValue oldValue, TValue newValue, Boolean hasChanged,
-            [CallerFilePath] String _file = null, [CallerMemberName] String _method = null) {
+        [TestParameters(typeof(String), null, null, false)]
+        [TestParameters(typeof(String), null, "new", true)]
+        [TestParameters(typeof(String), "old", null, true)]
+        [TestParameters(typeof(String), "old", "new", true)]
+        [TestParameters(typeof(String), "old", "old", false)]
+        [TestParameters(typeof(Int32), 5, 6, true)]
+        [TestParameters(typeof(Int32), 5, 5, false)]
+        void Constructor<TValue>(TValue oldValue, TValue newValue, Boolean hasChanged) {
 
             ValueChangedEventArgs<TValue> e = null;
 
-            Test.Note(String.Format("Test ctor with: '{0}', '{1}'", oldValue, newValue), _file, _method);
-            Test.IfNot.Action.ThrowsException(() => e = new ValueChangedEventArgs<TValue>(oldValue, newValue), out Exception ex, _file, _method);
-            Test.IfNot.Object.IsNull(e, _file, _method);
-            Test.If.Value.IsEqual(e.Old, oldValue, _file, _method);
-            Test.If.Value.IsEqual(e.New, newValue, _file, _method);
-            Test.If.Value.IsEqual(e.HasChanged, hasChanged, _file, _method);
+            Test.IfNot.Action.ThrowsException(() => e = new ValueChangedEventArgs<TValue>(oldValue, newValue), out Exception ex);
+            Test.IfNot.Object.IsNull(e);
+            Test.If.Value.IsEqual(e.Old, oldValue);
+            Test.If.Value.IsEqual(e.New, newValue);
+            Test.If.Value.IsEqual(e.HasChanged, hasChanged);
 
         }
 

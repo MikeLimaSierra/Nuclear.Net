0039596bb520da85cb5e447e2b4b16d260183653
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-04-30 22:25:11 +0200

    Update RuntimeInfo_uTests.cs

--- src/Nuclear.Assemblies.uTests/Runtimes/RuntimeInfo_uTests.cs
+++ src/Nuclear.Assemblies.uTests/Runtimes/RuntimeInfo_uTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+
 using Nuclear.Extensions;
 using Nuclear.TestSite;
 
@@ -9,30 +10,33 @@ namespace Nuclear.Assemblies.Runtimes {
         #region ctors
 
         [TestMethod]
-        void Constructor() {
+        void ConstructorThrows() {
 
             RuntimeInfo info = null;
 
             Test.If.Action.ThrowsException(() => info = new RuntimeInfo((FrameworkIdentifiers) 42, null), out ArgumentException argEx);
             Test.If.Action.ThrowsException(() => info = new RuntimeInfo(FrameworkIdentifiers.Unsupported, null), out ArgumentNullException argNullEx);
 
-            DDTConstructor((FrameworkIdentifiers.Unsupported, new Version(1, 2, 3)), (FrameworkIdentifiers.Unsupported, new Version(1, 2, 3)));
-            DDTConstructor((FrameworkIdentifiers.NETStandard, new Version(2, 3, 4)), (FrameworkIdentifiers.NETStandard, new Version(2, 3, 4)));
-
         }
 
-        void DDTConstructor((FrameworkIdentifiers framework, Version version) input, (FrameworkIdentifiers framework, Version version) expected,
-            [CallerFilePath] String _file = null, [CallerMemberName] String _method = null) {
+        [TestMethod]
+        [TestData(nameof(ConstructorData))]
+        void Constructor(FrameworkIdentifiers input1, Version input2, FrameworkIdentifiers framework, Version version) {
 
             RuntimeInfo info = null;
 
-            Test.Note($"new RuntimeInfo({input.framework.Format()}, {input.version.Format()})", _file, _method);
+            Test.IfNot.Action.ThrowsException(() => info = new RuntimeInfo(input1, input2), out Exception ex);
 
-            Test.IfNot.Action.ThrowsException(() => info = new RuntimeInfo(input.framework, input.version), out Exception ex, _file, _method);
+            Test.If.Value.IsEqual(info.Framework, framework);
+            Test.If.Value.IsEqual(info.Version, version);
 
-            Test.If.Value.IsEqual(info.Framework, expected.framework, _file, _method);
-            Test.If.Value.IsEqual(info.Version, expected.version, _file, _method);
+        }
 
+        IEnumerable<Object[]> ConstructorData() {
+            return new List<Object[]>() {
+                new Object[] { FrameworkIdentifiers.Unsupported, new Version(1, 2, 3), FrameworkIdentifiers.Unsupported, new Version(1, 2, 3) },
+                new Object[] { FrameworkIdentifiers.NETStandard, new Version(2, 3, 4), FrameworkIdentifiers.NETStandard, new Version(2, 3, 4) },
+            };
         }
 
         #endregion
@@ -40,45 +44,46 @@ namespace Nuclear.Assemblies.Runtimes {
         #region Equals
 
         [TestMethod]
-        void Equals() {
-
-            DDTEquals((new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), (Object) null), false);
-            DDTEquals((new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), "wrong type"), false);
-
-            DDTEquals((new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), (Object) new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0))), true);
-            DDTEquals((new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), (Object) new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 1))), false);
-            DDTEquals((new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), (Object) new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(2, 0))), false);
-            DDTEquals((new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), (Object) new RuntimeInfo(FrameworkIdentifiers.NETFramework, new Version(1, 0))), false);
-
-            DDTEquals((new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0))), true);
-            DDTEquals((new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 1))), false);
-            DDTEquals((new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(2, 0))), false);
-            DDTEquals((new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), new RuntimeInfo(FrameworkIdentifiers.NETFramework, new Version(1, 0))), false);
-
-        }
-
-        void DDTEquals((RuntimeInfo left, Object right) input, Boolean expected,
-            [CallerFilePath] String _file = null, [CallerMemberName] String _method = null) {
+        [TestData(nameof(EqualsObjectData))]
+        void EqualsObject(RuntimeInfo left, Object right, Boolean expected) {
 
             Boolean result = false;
 
-            Test.Note($"{input.left.Format()}.Equals({input.right.Format()}) == {expected.Format()}", _file, _method);
-
-            Test.IfNot.Action.ThrowsException(() => result = input.left.Equals(input.right), out Exception ex, _file, _method);
-            Test.If.Value.IsEqual(result, expected, _file, _method);
+            Test.IfNot.Action.ThrowsException(() => result = left.Equals(right), out Exception ex);
+            Test.If.Value.IsEqual(result, expected);
 
         }
 
-        void DDTEquals((RuntimeInfo left, RuntimeInfo right) input, Boolean expected,
-            [CallerFilePath] String _file = null, [CallerMemberName] String _method = null) {
+        IEnumerable<Object[]> EqualsObjectData() {
+            return new List<Object[]>() {
+                new Object[] { new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), null, false },
+                new Object[] { new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), "wrong type", false },
+
+                new Object[] { new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), true },
+                new Object[] { new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 1)), false },
+                new Object[] { new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(2, 0)), false },
+                new Object[] { new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), new RuntimeInfo(FrameworkIdentifiers.NETFramework, new Version(1, 0)), false },
+            };
+        }
+
+        [TestMethod]
+        [TestData(nameof(EqualsData))]
+        void Equals(RuntimeInfo left, RuntimeInfo right, Boolean expected) {
 
             Boolean result = false;
 
-            Test.Note($"{input.left.Format()}.Equals({input.right.Format()}) == {expected.Format()}", _file, _method);
+            Test.IfNot.Action.ThrowsException(() => result = left.Equals(right), out Exception ex);
+            Test.If.Value.IsEqual(result, expected);
 
-            Test.IfNot.Action.ThrowsException(() => result = input.left.Equals(input.right), out Exception ex, _file, _method);
-            Test.If.Value.IsEqual(result, expected, _file, _method);
+        }
 
+        IEnumerable<Object[]> EqualsData() {
+            return new List<Object[]>() {
+                new Object[] { new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), true },
+                new Object[] { new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 1)), false },
+                new Object[] { new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(2, 0)), false },
+                new Object[] { new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(1, 0)), new RuntimeInfo(FrameworkIdentifiers.NETFramework, new Version(1, 0)), false },
+            };
         }
 
         #endregion
@@ -86,25 +91,23 @@ namespace Nuclear.Assemblies.Runtimes {
         #region ToString
 
         [TestMethod]
-        new void ToString() {
-
-            DDTToString(new RuntimeInfo(FrameworkIdentifiers.NETFramework, new Version(1, 0)), "NETFramework 1.0");
-            DDTToString(new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(2, 1)), "NETCoreApp 2.1");
-            DDTToString(new RuntimeInfo(FrameworkIdentifiers.NETStandard, new Version(3, 2)), "NETStandard 3.2");
-            DDTToString(new RuntimeInfo(FrameworkIdentifiers.Unsupported, new Version(4, 3)), "Unsupported 4.3");
-
-        }
-
-        void DDTToString(RuntimeInfo input, String expected,
-            [CallerFilePath] String _file = null, [CallerMemberName] String _method = null) {
+        [TestData(nameof(ToStringData))]
+        void ToString(RuntimeInfo input, String expected) {
 
             String result = null;
 
-            Test.Note($"RuntimeInfo({input.Framework.Format()}, {input.Version.Format()}).ToString() == {expected.Format()}", _file, _method);
+            Test.IfNot.Action.ThrowsException(() => result = input.ToString(), out Exception ex);
+            Test.If.Value.IsEqual(result, expected);
 
-            Test.IfNot.Action.ThrowsException(() => result = input.ToString(), out Exception ex, _file, _method);
-            Test.If.Value.IsEqual(result, expected, _file, _method);
+        }
 
+        IEnumerable<Object[]> ToStringData() {
+            return new List<Object[]>() {
+                new Object[] { new RuntimeInfo(FrameworkIdentifiers.NETFramework, new Version(1, 0)), "NETFramework 1.0" },
+                new Object[] { new RuntimeInfo(FrameworkIdentifiers.NETCoreApp, new Version(2, 1)), "NETCoreApp 2.1" },
+                new Object[] { new RuntimeInfo(FrameworkIdentifiers.NETStandard, new Version(3, 2)), "NETStandard 3.2" },
+                new Object[] { new RuntimeInfo(FrameworkIdentifiers.Unsupported, new Version(4, 3)), "Unsupported 4.3" },
+            };
         }
 
         #endregion

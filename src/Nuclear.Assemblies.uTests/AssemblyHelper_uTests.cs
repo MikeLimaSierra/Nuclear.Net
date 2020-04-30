455d41cedab495bc77e59a4828c28798c7fb1652
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-04-30 22:37:55 +0200

    Update AssemblyHelper_uTests.cs

--- src/Nuclear.Assemblies.uTests/AssemblyHelper_uTests.cs
+++ src/Nuclear.Assemblies.uTests/AssemblyHelper_uTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+
 using Nuclear.Assemblies.Runtimes;
 using Nuclear.Extensions;
 using Nuclear.TestSite;
@@ -22,27 +24,25 @@ namespace Nuclear.Assemblies {
         #region runtime info
 
         [TestMethod]
-        void TryGetRuntime() {
+        [TestData(nameof(TryGetRuntimeData))]
+        void TryGetRuntime(Assembly input, Boolean result, RuntimeInfo runtime) {
 
-            DDTTryGetRuntime(null, (false, null));
-            DDTTryGetRuntime(typeof(TestMethodAttribute).Assembly, (true, new RuntimeInfo(FrameworkIdentifiers.NETStandard, new Version(1, 0))));
-            DDTTryGetRuntime(typeof(Test).Assembly, (true, new RuntimeInfo(FrameworkIdentifiers.NETStandard, new Version(2, 0))));
+            Boolean _result = default;
+            RuntimeInfo _runtime = default;
+
+            Test.IfNot.Action.ThrowsException(() => _result = AssemblyHelper.TryGetRuntime(input, out _runtime), out Exception ex);
+
+            Test.If.Value.IsEqual(_result, result);
+            Test.If.Value.IsEqual(_runtime, runtime);
 
         }
 
-        void DDTTryGetRuntime(Assembly input, (Boolean result, RuntimeInfo runtime) expected,
-            [CallerFilePath] String _file = null, [CallerMemberName] String _method = null) {
-
-            Boolean result = default;
-            RuntimeInfo runtime = default;
-
-            Test.Note($"AssemblyHelper.TryGetRuntime({input.Format()}, out {expected.runtime.Format()}) == {expected.result.Format()}", _file, _method);
-
-            Test.IfNot.Action.ThrowsException(() => result = AssemblyHelper.TryGetRuntime(input, out runtime), out Exception ex, _file, _method);
-
-            Test.If.Value.IsEqual(result, expected.result, _file, _method);
-            Test.If.Value.IsEqual(runtime, expected.runtime, _file, _method);
-
+        IEnumerable<Object[]> TryGetRuntimeData() {
+            return new List<Object[]>() {
+                new Object[] { null, false, null },
+                new Object[] { typeof(TestMethodAttribute).Assembly, true, new RuntimeInfo(FrameworkIdentifiers.NETStandard, new Version(1, 0)) },
+                new Object[] { typeof(Test).Assembly, true, new RuntimeInfo(FrameworkIdentifiers.NETStandard, new Version(2, 0)) },
+            };
         }
 
         #endregion

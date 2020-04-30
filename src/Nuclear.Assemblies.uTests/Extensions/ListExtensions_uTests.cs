c03c5572e7d39cc5a6f0320a4f658ef45c865487
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-04-30 21:44:13 +0200

    Update ListExtensions_uTests.cs

--- src/Nuclear.Assemblies.uTests/Extensions/ListExtensions_uTests.cs
+++ src/Nuclear.Assemblies.uTests/Extensions/ListExtensions_uTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Nuclear.Extensions;
+
 using Nuclear.TestSite;
 
 namespace Nuclear.Assemblies.Extensions {
@@ -18,28 +17,34 @@ namespace Nuclear.Assemblies.Extensions {
 
         }
 
-        [TestMethod]
         void TryTake() {
 
-            DDTTryTake((new List<String>() { "the quick fox", "jumps over", "the lazy dog" }, _ => _.StartsWith("jumps")), (true, "jumps over", new List<String>() { "the quick fox", "the lazy dog" }));
-            DDTTryTake((new List<String>() { "the quick fox", " jumps over", "the lazy dog" }, _ => _.StartsWith("jumps")), (false, null, new List<String>() { "the quick fox", " jumps over", "the lazy dog" }));
-            DDTTryTake((new List<String>() { "the quick fox", "jumps over", "the lazy dog" }, _ => _.StartsWith(" jumps")), (false, null, new List<String>() { "the quick fox", "jumps over", "the lazy dog" }));
+            TryTake(new List<String>() { "the quick fox", "jumps over", "the lazy dog" }, _ => _.StartsWith("jumps"), true, "jumps over", new List<String>() { "the quick fox", "the lazy dog" });
+            TryTake(new List<String>() { "the quick fox", " jumps over", "the lazy dog" }, _ => _.StartsWith("jumps"), false, null, new List<String>() { "the quick fox", " jumps over", "the lazy dog" });
+            TryTake(new List<String>() { "the quick fox", "jumps over", "the lazy dog" }, _ => _.StartsWith(" jumps"), false, null, new List<String>() { "the quick fox", "jumps over", "the lazy dog" });
 
         }
 
-        void DDTTryTake<T>((List<T> list, Predicate<T> match) input, (Boolean result, T element, List<T> list) expected,
-            [CallerFilePath] String _file = null, [CallerMemberName] String _method = null) {
+        [TestMethod]
+        [TestData(nameof(TryTakeData))]
+        void TryTake<T>(List<T> input1, Predicate<T> input2, Boolean result, T element, List<T> list) {
 
-            Boolean result = false;
-            T element = default;
+            Boolean _result = false;
+            T _element = default;
 
-            Test.Note($"{input.list.Format()}.TryTake(match, out {expected.element.Format()}) == {expected.result.Format()} => {expected.list.Format()}", _file, _method);
+            Test.IfNot.Action.ThrowsException(() => _result = input1.TryTake(input2, out _element), out Exception ex);
+            Test.If.Value.IsEqual(_result, result);
+            Test.If.Value.IsEqual(_element, element);
+            Test.If.Enumerable.Matches(input1, list);
 
-            Test.IfNot.Action.ThrowsException(() => result = input.list.TryTake(input.match, out element), out Exception ex, _file, _method);
-            Test.If.Value.IsEqual(result, expected.result, _file, _method);
-            Test.If.Value.IsEqual(element, expected.element, _file, _method);
-            Test.If.Enumerable.Matches(input.list, expected.list, _file, _method);
+        }
 
+        IEnumerable<Object[]> TryTakeData() {
+            return new List<Object[]>() {
+                new Object[] { typeof(String), new List<String>() { "the quick fox", "jumps over", "the lazy dog" }, new Predicate<String>((_) => _.StartsWith("jumps")), true, "jumps over", new List<String>() { "the quick fox", "the lazy dog" } },
+                new Object[] { typeof(String), new List<String>() { "the quick fox", " jumps over", "the lazy dog" }, new Predicate<String>((_) => _.StartsWith("jumps")), false, null, new List<String>() { "the quick fox", " jumps over", "the lazy dog" } },
+                new Object[] { typeof(String), new List<String>() { "the quick fox", "jumps over", "the lazy dog" }, new Predicate<String>((_) => _.StartsWith(" jumps")), false, null, new List<String>() { "the quick fox", "jumps over", "the lazy dog" } },
+            };
         }
 
     }

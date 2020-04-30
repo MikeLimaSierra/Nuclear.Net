608d8d3040d19f0c46b95626fe2ffb4886d86ce9
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-04-30 21:49:13 +0200

    Update FrameworkIdentifiersComparer_uTests.cs

--- src/Nuclear.Assemblies.uTests/Runtimes/FrameworkIdentifiersComparer_uTests.cs
+++ src/Nuclear.Assemblies.uTests/Runtimes/FrameworkIdentifiersComparer_uTests.cs
@@ -15,53 +15,75 @@ namespace Nuclear.Assemblies.Runtimes {
 
         }
 
-        [TestMethod]
         void Compare() {
 
-            DDTCompare(((FrameworkIdentifiers) 1337, (FrameworkIdentifiers) 1337), 0);
-            DDTCompare(((FrameworkIdentifiers) 1337, (FrameworkIdentifiers) 1338), 0);
-            DDTCompare(((FrameworkIdentifiers) 1339, (FrameworkIdentifiers) 1338), 0);
+            Compare((FrameworkIdentifiers) 1337, (FrameworkIdentifiers) 1337, 0);
+            Compare((FrameworkIdentifiers) 1337, (FrameworkIdentifiers) 1338, 0);
+            Compare((FrameworkIdentifiers) 1339, (FrameworkIdentifiers) 1338, 0);
 
-            DDTCompare((FrameworkIdentifiers.Unsupported, (FrameworkIdentifiers) 1337), 1);
-            DDTCompare(((FrameworkIdentifiers) 1337, FrameworkIdentifiers.Unsupported), -1);
-            DDTCompare((FrameworkIdentifiers.NETStandard, (FrameworkIdentifiers) 1337), 1);
-            DDTCompare(((FrameworkIdentifiers) 1337, FrameworkIdentifiers.NETStandard), -1);
-            DDTCompare((FrameworkIdentifiers.NETFramework, (FrameworkIdentifiers) 1337), 1);
-            DDTCompare(((FrameworkIdentifiers) 1337, FrameworkIdentifiers.NETFramework), -1);
-            DDTCompare((FrameworkIdentifiers.NETCoreApp, (FrameworkIdentifiers) 1337), 1);
-            DDTCompare(((FrameworkIdentifiers) 1337, FrameworkIdentifiers.NETCoreApp), -1);
+            Compare(FrameworkIdentifiers.Unsupported, (FrameworkIdentifiers) 1337, 1);
+            Compare((FrameworkIdentifiers) 1337, FrameworkIdentifiers.Unsupported, -1);
+            Compare(FrameworkIdentifiers.NETStandard, (FrameworkIdentifiers) 1337, 1);
+            Compare((FrameworkIdentifiers) 1337, FrameworkIdentifiers.NETStandard, -1);
+            Compare(FrameworkIdentifiers.NETFramework, (FrameworkIdentifiers) 1337, 1);
+            Compare((FrameworkIdentifiers) 1337, FrameworkIdentifiers.NETFramework, -1);
+            Compare(FrameworkIdentifiers.NETCoreApp, (FrameworkIdentifiers) 1337, 1);
+            Compare((FrameworkIdentifiers) 1337, FrameworkIdentifiers.NETCoreApp, -1);
 
-            DDTCompare((FrameworkIdentifiers.NETFramework, FrameworkIdentifiers.Unsupported), 1);
-            DDTCompare((FrameworkIdentifiers.NETCoreApp, FrameworkIdentifiers.Unsupported), 1);
-            DDTCompare((FrameworkIdentifiers.NETStandard, FrameworkIdentifiers.Unsupported), 1);
+            Compare(FrameworkIdentifiers.NETFramework, FrameworkIdentifiers.Unsupported, 1);
+            Compare(FrameworkIdentifiers.NETCoreApp, FrameworkIdentifiers.Unsupported, 1);
+            Compare(FrameworkIdentifiers.NETStandard, FrameworkIdentifiers.Unsupported, 1);
 
-            DDTCompare((FrameworkIdentifiers.Unsupported, FrameworkIdentifiers.NETFramework), -1);
-            DDTCompare((FrameworkIdentifiers.Unsupported, FrameworkIdentifiers.NETCoreApp), -1);
-            DDTCompare((FrameworkIdentifiers.Unsupported, FrameworkIdentifiers.NETStandard), -1);
+            Compare(FrameworkIdentifiers.Unsupported, FrameworkIdentifiers.NETFramework, -1);
+            Compare(FrameworkIdentifiers.Unsupported, FrameworkIdentifiers.NETCoreApp, -1);
+            Compare(FrameworkIdentifiers.Unsupported, FrameworkIdentifiers.NETStandard, -1);
 
-            DDTCompare((FrameworkIdentifiers.Unsupported, FrameworkIdentifiers.Unsupported), 0);
-            DDTCompare((FrameworkIdentifiers.NETFramework, FrameworkIdentifiers.NETFramework), 0);
-            DDTCompare((FrameworkIdentifiers.NETCoreApp, FrameworkIdentifiers.NETCoreApp), 0);
-            DDTCompare((FrameworkIdentifiers.NETStandard, FrameworkIdentifiers.NETStandard), 0);
+            Compare(FrameworkIdentifiers.Unsupported, FrameworkIdentifiers.Unsupported, 0);
+            Compare(FrameworkIdentifiers.NETFramework, FrameworkIdentifiers.NETFramework, 0);
+            Compare(FrameworkIdentifiers.NETCoreApp, FrameworkIdentifiers.NETCoreApp, 0);
+            Compare(FrameworkIdentifiers.NETStandard, FrameworkIdentifiers.NETStandard, 0);
 
-            DDTCompare((FrameworkIdentifiers.NETFramework, FrameworkIdentifiers.NETStandard), 1);
-            DDTCompare((FrameworkIdentifiers.NETStandard, FrameworkIdentifiers.NETFramework), -1);
-            DDTCompare((FrameworkIdentifiers.NETCoreApp, FrameworkIdentifiers.NETStandard), 1);
-            DDTCompare((FrameworkIdentifiers.NETStandard, FrameworkIdentifiers.NETCoreApp), -1);
+            Compare(FrameworkIdentifiers.NETFramework, FrameworkIdentifiers.NETStandard, 1);
+            Compare(FrameworkIdentifiers.NETStandard, FrameworkIdentifiers.NETFramework, -1);
+            Compare(FrameworkIdentifiers.NETCoreApp, FrameworkIdentifiers.NETStandard, 1);
+            Compare(FrameworkIdentifiers.NETStandard, FrameworkIdentifiers.NETCoreApp, -1);
 
         }
 
-        void DDTCompare((FrameworkIdentifiers x, FrameworkIdentifiers y) input, Int32 expected,
-            [CallerFilePath] String _file = null, [CallerMemberName] String _method = null) {
+        [TestMethod]
+        [TestParameters((FrameworkIdentifiers) 1337, (FrameworkIdentifiers) 1337, 0)]
+        [TestParameters((FrameworkIdentifiers) 1337, (FrameworkIdentifiers) 1338, 0)]
+        [TestParameters((FrameworkIdentifiers) 1339, (FrameworkIdentifiers) 1338, 0)]
+        [TestParameters(FrameworkIdentifiers.Unsupported, (FrameworkIdentifiers) 1337, 1)]
+        [TestParameters((FrameworkIdentifiers) 1337, FrameworkIdentifiers.Unsupported, -1)]
+        [TestParameters(FrameworkIdentifiers.NETStandard, (FrameworkIdentifiers) 1337, 1)]
+        [TestParameters((FrameworkIdentifiers) 1337, FrameworkIdentifiers.NETStandard, -1)]
+        [TestParameters(FrameworkIdentifiers.NETFramework, (FrameworkIdentifiers) 1337, 1)]
+        [TestParameters((FrameworkIdentifiers) 1337, FrameworkIdentifiers.NETFramework, -1)]
+        [TestParameters(FrameworkIdentifiers.NETCoreApp, (FrameworkIdentifiers) 1337, 1)]
+        [TestParameters((FrameworkIdentifiers) 1337, FrameworkIdentifiers.NETCoreApp, -1)]
+        [TestParameters(FrameworkIdentifiers.NETFramework, FrameworkIdentifiers.Unsupported, 1)]
+        [TestParameters(FrameworkIdentifiers.NETCoreApp, FrameworkIdentifiers.Unsupported, 1)]
+        [TestParameters(FrameworkIdentifiers.NETStandard, FrameworkIdentifiers.Unsupported, 1)]
+        [TestParameters(FrameworkIdentifiers.Unsupported, FrameworkIdentifiers.NETFramework, -1)]
+        [TestParameters(FrameworkIdentifiers.Unsupported, FrameworkIdentifiers.NETCoreApp, -1)]
+        [TestParameters(FrameworkIdentifiers.Unsupported, FrameworkIdentifiers.NETStandard, -1)]
+        [TestParameters(FrameworkIdentifiers.Unsupported, FrameworkIdentifiers.Unsupported, 0)]
+        [TestParameters(FrameworkIdentifiers.NETFramework, FrameworkIdentifiers.NETFramework, 0)]
+        [TestParameters(FrameworkIdentifiers.NETCoreApp, FrameworkIdentifiers.NETCoreApp, 0)]
+        [TestParameters(FrameworkIdentifiers.NETStandard, FrameworkIdentifiers.NETStandard, 0)]
+        [TestParameters(FrameworkIdentifiers.NETFramework, FrameworkIdentifiers.NETStandard, 1)]
+        [TestParameters(FrameworkIdentifiers.NETStandard, FrameworkIdentifiers.NETFramework, -1)]
+        [TestParameters(FrameworkIdentifiers.NETCoreApp, FrameworkIdentifiers.NETStandard, 1)]
+        [TestParameters(FrameworkIdentifiers.NETStandard, FrameworkIdentifiers.NETCoreApp, -1)]
+        void Compare(FrameworkIdentifiers input1, FrameworkIdentifiers input2, Int32 expected) {
 
             IComparer<FrameworkIdentifiers> comparer = new FrameworkIdentifiersComparer();
             Int32 result = default;
 
-            Test.Note($"FrameworkIdentifiersComparer.Compare({input.x.Format()}, {input.y.Format()}) == {expected.Format()}", _file, _method);
+            Test.IfNot.Action.ThrowsException(() => result = comparer.Compare(input1, input2), out Exception ex);
 
-            Test.IfNot.Action.ThrowsException(() => result = comparer.Compare(input.x, input.y), out Exception ex, _file, _method);
-
-            Test.If.Value.IsEqual(result, expected, _file, _method);
+            Test.If.Value.IsEqual(result, expected);
 
         }
 

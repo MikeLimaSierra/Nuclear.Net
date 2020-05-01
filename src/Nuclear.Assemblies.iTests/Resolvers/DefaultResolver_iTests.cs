b0a4f21e3c03e84823892f50e40c559fa4eb623c
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-05-01 14:38:30 +0200

    Update DefaultResolver_iTests.cs

--- src/Nuclear.Assemblies.iTests/Resolvers/DefaultResolver_iTests.cs
+++ src/Nuclear.Assemblies.iTests/Resolvers/DefaultResolver_iTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
+
 using Nuclear.Extensions;
 using Nuclear.TestSite;
 
@@ -13,82 +13,86 @@ namespace Nuclear.Assemblies.Resolvers {
         #region TryResolve
 
         [TestMethod]
-        void TryResolve() {
+        [TestData(nameof(TryResolveArgsData))]
+        void TryResolveArgs(ResolveEventArgs input, Boolean result, IEnumerable<FileInfo> files) {
 
-            DDTTryResolve((ResolveEventArgs) null, (false, Enumerable.Empty<FileInfo>()));
-            DDTTryResolve(new ResolveEventArgs(null, null), (false, Enumerable.Empty<FileInfo>()));
-            DDTTryResolve(new ResolveEventArgs("", null), (false, Enumerable.Empty<FileInfo>()));
-            DDTTryResolve(new ResolveEventArgs("some name", null), (false, Enumerable.Empty<FileInfo>()));
-            DDTTryResolve(new ResolveEventArgs(typeof(DefaultResolver_iTests).Assembly.FullName, null), (false, Enumerable.Empty<FileInfo>()));
-            DDTTryResolve(new ResolveEventArgs(typeof(StringExtensions).Assembly.FullName, null), (true, new FileInfo[] {
-                new FileInfo(Path.Combine(Statics.EntryPath.DirectoryName, "Nuclear.Extensions.dll"))
-            }));
-            DDTTryResolve(new ResolveEventArgs(typeof(DefaultResolver).Assembly.FullName, Statics.TestAsm), (true, new FileInfo[] {
-                new FileInfo(Path.Combine(Statics.TestPath.DirectoryName, "Nuclear.Assemblies.dll"))
-            }));
+            IDefaultResolver instance = DefaultResolver.Instance;
+            Boolean _result = false;
+            IEnumerable<FileInfo> _files = null;
 
-            DDTTryResolve((String) null, (false, Enumerable.Empty<FileInfo>()));
-            DDTTryResolve("", (false, Enumerable.Empty<FileInfo>()));
-            DDTTryResolve("some name", (false, Enumerable.Empty<FileInfo>()));
-            DDTTryResolve(typeof(DefaultResolver_iTests).Assembly.FullName, (false, Enumerable.Empty<FileInfo>()));
-            DDTTryResolve(typeof(StringExtensions).Assembly.FullName, (true, new FileInfo[] {
-                new FileInfo(Path.Combine(Statics.EntryPath.DirectoryName, "Nuclear.Extensions.dll"))
-            }));
+            Test.IfNot.Action.ThrowsException(() => _result = instance.TryResolve(input, out _files), out Exception ex);
 
-            DDTTryResolve((AssemblyName) null, (false, Enumerable.Empty<FileInfo>()));
-            DDTTryResolve(typeof(DefaultResolver_iTests).Assembly.GetName(), (false, Enumerable.Empty<FileInfo>()));
-            DDTTryResolve(typeof(StringExtensions).Assembly.GetName(), (true, new FileInfo[] {
-                new FileInfo(Path.Combine(Statics.EntryPath.DirectoryName, "Nuclear.Extensions.dll"))
-            }));
+            Test.If.Value.IsEqual(_result, result);
+            Test.If.Enumerable.Matches(_files, files, Statics.FileInfoComparer);
 
         }
 
-        void DDTTryResolve(ResolveEventArgs input, (Boolean result, IEnumerable<FileInfo> files) expected,
-            [CallerFilePath] String _file = null, [CallerMemberName] String _method = null) {
+        IEnumerable<Object[]> TryResolveArgsData() {
+            return new List<Object[]>() {
+                new Object[] { null, false, Enumerable.Empty<FileInfo>() },
+                new Object[] { new ResolveEventArgs(null, null), false, Enumerable.Empty<FileInfo>() },
+                new Object[] { new ResolveEventArgs("", null), false, Enumerable.Empty<FileInfo>() },
+                new Object[] { new ResolveEventArgs("some name", null), false, Enumerable.Empty<FileInfo>() },
+                new Object[] { new ResolveEventArgs(typeof(DefaultResolver_iTests).Assembly.FullName, null), false, Enumerable.Empty<FileInfo>() },
+                new Object[] { new ResolveEventArgs(typeof(StringExtensions).Assembly.FullName, null), true, new FileInfo[] {
+                    new FileInfo(Path.Combine(Statics.EntryPath.DirectoryName, "Nuclear.Extensions.dll"))
+                } },
+                new Object[] { new ResolveEventArgs(typeof(DefaultResolver).Assembly.FullName, Statics.TestAsm), true, new FileInfo[] {
+                    new FileInfo(Path.Combine(Statics.TestPath.DirectoryName, "Nuclear.Assemblies.dll"))
+                } },
+            };
+        }
+
+        [TestMethod]
+        [TestData(nameof(TryResolveStringData))]
+        void TryResolveString(String input, Boolean result, IEnumerable<FileInfo> files) {
 
             IDefaultResolver instance = DefaultResolver.Instance;
-            Boolean result = false;
-            IEnumerable<FileInfo> files = null;
+            Boolean _result = false;
+            IEnumerable<FileInfo> _files = null;
 
-            Test.Note($"DefaultResolver.TryResolve({input.Format()}, out {expected.files.Format()}) == {expected.result.Format()}", _file, _method);
+            Test.IfNot.Action.ThrowsException(() => _result = instance.TryResolve(input, out _files), out Exception ex);
 
-            Test.IfNot.Action.ThrowsException(() => result = instance.TryResolve(input, out files), out Exception ex, _file, _method);
-
-            Test.If.Value.IsEqual(result, expected.result, _file, _method);
-            Test.If.Enumerable.Matches(files, expected.files, Statics.FileInfoComparer, _file, _method);
+            Test.If.Value.IsEqual(_result, result);
+            Test.If.Enumerable.Matches(_files, files, Statics.FileInfoComparer);
 
         }
 
-        void DDTTryResolve(String input, (Boolean result, IEnumerable<FileInfo> files) expected,
-            [CallerFilePath] String _file = null, [CallerMemberName] String _method = null) {
+        IEnumerable<Object[]> TryResolveStringData() {
+            return new List<Object[]>() {
+                new Object[] { null, false, Enumerable.Empty<FileInfo>() },
+                new Object[] { "", false, Enumerable.Empty<FileInfo>() },
+                new Object[] { "some name", false, Enumerable.Empty<FileInfo>() },
+                new Object[] { typeof(DefaultResolver_iTests).Assembly.FullName, false, Enumerable.Empty<FileInfo>() },
+                new Object[] { typeof(StringExtensions).Assembly.FullName, true, new FileInfo[] {
+                    new FileInfo(Path.Combine(Statics.EntryPath.DirectoryName, "Nuclear.Extensions.dll"))
+                } },
+            };
+        }
+
+        [TestMethod]
+        [TestData(nameof(TryResolveNameData))]
+        void TryResolveName(AssemblyName input, Boolean result, IEnumerable<FileInfo> files) {
 
             IDefaultResolver instance = DefaultResolver.Instance;
-            Boolean result = false;
-            IEnumerable<FileInfo> files = null;
+            Boolean _result = false;
+            IEnumerable<FileInfo> _files = null;
 
-            Test.Note($"DefaultResolver.TryResolve({input.Format()}, out {expected.files.Format()}) == {expected.result.Format()}", _file, _method);
+            Test.IfNot.Action.ThrowsException(() => _result = instance.TryResolve(input, out _files), out Exception ex);
 
-            Test.IfNot.Action.ThrowsException(() => result = instance.TryResolve(input, out files), out Exception ex, _file, _method);
-
-            Test.If.Value.IsEqual(result, expected.result, _file, _method);
-            Test.If.Enumerable.Matches(files, expected.files, Statics.FileInfoComparer, _file, _method);
+            Test.If.Value.IsEqual(_result, result);
+            Test.If.Enumerable.Matches(_files, files, Statics.FileInfoComparer);
 
         }
 
-        void DDTTryResolve(AssemblyName input, (Boolean result, IEnumerable<FileInfo> files) expected,
-            [CallerFilePath] String _file = null, [CallerMemberName] String _method = null) {
-
-            IDefaultResolver instance = DefaultResolver.Instance;
-            Boolean result = false;
-            IEnumerable<FileInfo> files = null;
-
-            Test.Note($"DefaultResolver.TryResolve({input.Format()}, out {expected.files.Format()}) == {expected.result.Format()}", _file, _method);
-
-            Test.IfNot.Action.ThrowsException(() => result = instance.TryResolve(input, out files), out Exception ex, _file, _method);
-
-            Test.If.Value.IsEqual(result, expected.result, _file, _method);
-            Test.If.Enumerable.Matches(files, expected.files, Statics.FileInfoComparer, _file, _method);
-
+        IEnumerable<Object[]> TryResolveNameData() {
+            return new List<Object[]>() {
+                new Object[] { null, false, Enumerable.Empty<FileInfo>() },
+                new Object[] { typeof(DefaultResolver_iTests).Assembly.GetName(), false, Enumerable.Empty<FileInfo>() },
+                new Object[] { typeof(StringExtensions).Assembly.GetName(), true, new FileInfo[] {
+                    new FileInfo(Path.Combine(Statics.EntryPath.DirectoryName, "Nuclear.Extensions.dll"))
+                } },
+            };
         }
 
         #endregion

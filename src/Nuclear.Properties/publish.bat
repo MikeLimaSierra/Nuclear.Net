1f61c241b6d9c6b96ec7800ec20ae382de25fc72
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-06-07 14:28:03 +0200

    add publishing

---
+++ src/Nuclear.Properties/publish.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+
+SET sn_exe="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\sn.exe"
+SET key=..\KeyPair.snk
+SET bin=..\..\bin\Nuclear.Properties\AnyCPU\Release\netstandard1.0\
+SET publish=..\..\publish\Nuclear.Properties\netstandard1.0\
+SET dll=Nuclear.Properties.dll
+SET pdb=Nuclear.Properties.pdb
+SET xml=Nuclear.Properties.xml
+SET deps=Nuclear.Properties.deps.json
+
+REM delete publish dir
+RMDIR /S /Q %publish%
+
+REM resign assembly
+%sn_exe% -Ra %bin%%dll% %key%
+
+REM verify assembly in bin
+%sn_exe% -vf %bin%%dll%
+
+REM copy output to publish dir
+robocopy %bin% %publish% %dll% %pdb% %xml% %deps%
+
+REM verify assembly in publish
+%sn_exe% -vf %publish%%dll%

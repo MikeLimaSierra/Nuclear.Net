cd1f5d47c0c227be86d37306e83240e68f72b99a
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-05-19 21:30:53 +0200

    make publish script dynamic

--- src/publish.bat
+++ src/publish.bat
@@ -1,13 +1,24 @@
 @ECHO OFF
 
+CD /D "%~dp0"
+
 SET sn_exe="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\sn.exe"
-SET key=..\KeyPair.snk
-SET bin=..\..\bin\Nuclear.Arguments\AnyCPU\Release\netstandard2.0\
-SET publish=..\..\publish\Nuclear.Arguments\netstandard2.0\
-SET dll=Nuclear.Arguments.dll
-SET pdb=Nuclear.Arguments.pdb
-SET xml=Nuclear.Arguments.xml
-SET deps=Nuclear.Arguments.deps.json
+SET key=KeyPair.snk
+SET tfm=%1
+SET ns=%2
+SET asm=%3
+
+SET bin=..\bin\%ns%\AnyCPU\Release\%tfm%\
+SET publish=..\publish\%ns%\%tfm%\
+SET dll=%asm%.dll
+SET pdb=%asm%.pdb
+SET xml=%asm%.xml
+SET deps=%asm%.deps.json
+
+ECHO "### Publishing ###"
+ECHO "Assembly: %asm%.dll"
+ECHO "Bin: %bin%"
+ECHO "Pub: %publish%"
 
 REM delete publish dir
 RMDIR /S /Q %publish%

315c43bf9f9d9e6f76a26777a1f2c77ae82386f5
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-05-20 23:16:52 +0200

    Create DefaultResolverData.cs

---
+++ src/Nuclear.Assemblies/Resolvers/Data/DefaultResolverData.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+using Nuclear.Exceptions;
+
+namespace Nuclear.Assemblies.Resolvers.Data {
+
+    internal class DefaultResolverData : IDefaultResolverData {
+
+        #region properties
+
+        public FileInfo File { get; }
+
+        #endregion
+
+        #region ctors
+
+        internal DefaultResolverData(FileInfo file) {
+            Throw.If.Object.IsNull(file, nameof(file));
+
+            File = file;
+        }
+
+        #endregion
+
+    }
+
+}

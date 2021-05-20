317c9ec43e59c8775846546d9dfc34a000554aa6
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-05-20 23:11:29 +0200

    add resolver data

---
+++ src/Nuclear.Assemblies/Resolvers/Data/IAssemblyResolverData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Nuclear.Assemblies.Resolvers.Data {
+
+    /// <summary>
+    /// Defines the assembly information that was found by an <see cref="IAssemblyResolver{TData}"/>.
+    /// </summary>
+    public interface IAssemblyResolverData {
+
+        #region properties
+
+        /// <summary>
+        /// Gets the resolved assemblies.
+        /// </summary>
+        public IEnumerable<FileInfo> Files { get; }
+
+        #endregion
+
+    }
+
+}
--- src/Nuclear.Assemblies/Resolvers/IAssemblyResolver.cs
+++ src/Nuclear.Assemblies/Resolvers/IAssemblyResolver.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Reflection;
+
+using Nuclear.Assemblies.Resolvers.Data;
 
 namespace Nuclear.Assemblies.Resolvers {
 
     /// <summary>
     /// Defines an assembly resolver.
     /// </summary>
-    public interface IAssemblyResolver {
+    /// <typeparam name="TData">The type of the reslver data, must inherit from <see cref="IAssemblyResolverData"/>.</typeparam>
+    public interface IAssemblyResolver<TData> where TData : IAssemblyResolverData {
 
         #region methods
 
@@ -17,27 +18,27 @@ namespace Nuclear.Assemblies.Resolvers {
         /// A return value indicates if the resolving operation was successful.
         /// </summary>
         /// <param name="e">The given <see cref="ResolveEventArgs"/>.</param>
-        /// <param name="files">The resolved files.</param>
+        /// <param name="data">The resolved data.</param>
         /// <returns>True if a file could be found.</returns>
-        Boolean TryResolve(ResolveEventArgs e, out IEnumerable<FileInfo> files);
+        Boolean TryResolve(ResolveEventArgs e, out TData data);
 
         /// <summary>
         /// Resolves a reference assembly by the full name.
         /// A return value indicates if the resolving operation was successful.
         /// </summary>
         /// <param name="fullName">The full name of the assembly.</param>
-        /// <param name="files">The resolved files.</param>
+        /// <param name="data">The resolved data.</param>
         /// <returns>True if a file could be found.</returns>
-        Boolean TryResolve(String fullName, out IEnumerable<FileInfo> files);
+        Boolean TryResolve(String fullName, out TData data);
 
         /// <summary>
         /// Resolves a reference assembly by an <see cref="AssemblyName"/>.
         /// A return value indicates if the resolving operation was successful.
         /// </summary>
         /// <param name="assemblyName">The <see cref="AssemblyName"/> of the assembly.</param>
-        /// <param name="files">The resolved files.</param>
+        /// <param name="data">The resolved data.</param>
         /// <returns>True if a file could be found.</returns>
-        Boolean TryResolve(AssemblyName assemblyName, out IEnumerable<FileInfo> files);
+        Boolean TryResolve(AssemblyName assemblyName, out TData data);
 
         #endregion
 

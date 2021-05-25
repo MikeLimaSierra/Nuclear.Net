3b6c4441ede0762100f86cd16a7b4e5cfc2828d4
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-05-25 19:15:48 +0200

    add tests

---
+++ src/Nuclear.Creation.uTests/InternalFactory_uTests.cs
@@ -0,0 +1,14 @@
+﻿using Nuclear.TestSite;
+
+namespace Nuclear.Creation {
+    class InternalFactory_uTests {
+
+        [TestMethod]
+        void Implementation() {
+
+            Test.If.Type.Implements<InternalFactory, IFactory>();
+
+        }
+
+    }
+}

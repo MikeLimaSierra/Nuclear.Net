baed8ae1746a476597841b7829de3a4e884f42a3
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-06-01 19:57:58 +0200

    Create AssemblyResolverData.cs

---
+++ src/Nuclear.Assemblies/Resolvers/Data/AssemblyResolverData.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+
+using Nuclear.Exceptions;
+
+namespace Nuclear.Assemblies.Resolvers.Data {
+
+    /// <summary>
+    /// Implements the assembly information that was found by an <see cref="IAssemblyResolver{TData}"/>.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class AssemblyResolverData : IAssemblyResolverData {
+
+        #region properties
+
+        /// <summary>
+        /// Gets the resolved assembly.
+        /// </summary>
+        public FileInfo File { get; }
+
+        /// <summary>
+        /// Gets the <see cref="AssemblyName"/> of the assembly.
+        /// </summary>
+        public AssemblyName Name { get; private set; }
+
+        #endregion
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="AssemblyResolverData"/>.
+        /// </summary>
+        /// <param name="file">The assembly file that was resolved.</param>
+        protected internal AssemblyResolverData(FileInfo file) {
+            Throw.If.Object.IsNull(file, nameof(file));
+
+            File = file;
+
+            Init();
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Initializes the data object by analyzing the <see cref="File"/> object.
+        /// </summary>
+        protected virtual void Init() {
+            Throw.If.Value.IsFalse(AssemblyHelper.TryGetAssemblyName(File, out AssemblyName name), nameof(File), "Could not resolve the AssemblyName object.");
+
+            Name = name;
+        }
+
+        #endregion
+
+    }
+}

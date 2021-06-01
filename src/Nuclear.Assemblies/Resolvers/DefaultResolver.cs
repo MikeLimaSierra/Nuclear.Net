e3c27d8cc2ba896598fcd832c87175480b56da1b
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-06-01 19:58:44 +0200

    Update DefaultResolver.cs

--- src/Nuclear.Assemblies/Resolvers/DefaultResolver.cs
+++ src/Nuclear.Assemblies/Resolvers/DefaultResolver.cs
@@ -4,23 +4,17 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 
+using Nuclear.Assemblies.Factories;
 using Nuclear.Assemblies.Resolvers.Data;
-using Nuclear.Exceptions;
+using Nuclear.Creation;
 
 namespace Nuclear.Assemblies.Resolvers {
+
     internal class DefaultResolver : AssemblyResolver<IDefaultResolverData>, IDefaultResolver {
 
-        #region properties
+        #region fields
 
-        internal static IDefaultResolver Instance { get; } = new DefaultResolver();
-
-        #endregion
-
-        #region ctors
-
-        internal DefaultResolver() {
-            Throw.IfNot.Object.IsNull<AccessViolationException>(Instance, "Constructor must not be called twice.");
-        }
+        private static readonly ICreator<IDefaultResolverData, FileInfo> _factory = Factory.Instance.Default();
 
         #endregion
 
@@ -31,9 +25,15 @@ namespace Nuclear.Assemblies.Resolvers {
 
             if(AssemblyHelper.TryGetAssemblyName(e, out AssemblyName assemblyName)) {
                 if(e.RequestingAssembly == null) {
-                    data = ResolveInternal(assemblyName).Select(_ => new DefaultResolverData(_));
+                    data = ResolveInternal(assemblyName).Select(_ => {
+                        _factory.Create(out IDefaultResolverData data, _);
+                        return data;
+                    });
                 } else {
-                    data = ResolveInternal(assemblyName, new FileInfo(e.RequestingAssembly.Location).Directory, SearchOption.AllDirectories).Select(_ => new DefaultResolverData(_));
+                    data = ResolveInternal(assemblyName, new FileInfo(e.RequestingAssembly.Location).Directory, SearchOption.AllDirectories).Select(_ => {
+                        _factory.Create(out IDefaultResolverData data, _);
+                        return data;
+                    });
                 }
             }
 
@@ -50,7 +50,10 @@ namespace Nuclear.Assemblies.Resolvers {
             data = Enumerable.Empty<IDefaultResolverData>();
 
             if(assemblyName != null) {
-                data = ResolveInternal(assemblyName).Select(_ => new DefaultResolverData(_));
+                data = ResolveInternal(assemblyName).Select(_ => {
+                    _factory.Create(out IDefaultResolverData data, _);
+                    return data;
+                });
             }
 
             return data != null && data.Count() > 0;
@@ -58,5 +61,48 @@ namespace Nuclear.Assemblies.Resolvers {
 
         #endregion
 
+        #region private methods
+
+        internal static IEnumerable<FileInfo> ResolveInternal(AssemblyName assemblyName)
+            => ResolveInternal(assemblyName, SearchOption.AllDirectories);
+
+        internal static IEnumerable<FileInfo> ResolveInternal(AssemblyName assemblyName, DirectoryInfo searchDir)
+            => ResolveInternal(assemblyName, searchDir, SearchOption.AllDirectories);
+
+        internal static IEnumerable<FileInfo> ResolveInternal(AssemblyName assemblyName, SearchOption searchOption)
+           => ResolveInternal(assemblyName, new FileInfo(Assembly.GetEntryAssembly().Location).Directory, searchOption);
+
+        internal static IEnumerable<FileInfo> ResolveInternal(AssemblyName assemblyName, DirectoryInfo searchDir, SearchOption searchOption) {
+            if(searchDir == null || !searchDir.Exists) {
+                return ResolveInternal(assemblyName, searchOption);
+            }
+
+            if(!Enum.IsDefined(typeof(SearchOption), searchOption)) {
+                return ResolveInternal(assemblyName, searchDir);
+            }
+
+            List<FileInfo> files = new List<FileInfo>();
+
+            if(assemblyName != null) {
+
+                foreach(String extension in AssemblyHelper.AssemblyFileExtensions) {
+                    foreach(FileInfo file in searchDir.GetFiles($"{assemblyName.Name}.{extension}", searchOption)) {
+
+                        if(AssemblyHelper.TryGetAssemblyName(file, out AssemblyName _asmName)
+                            && AssemblyHelper.ValidateByName(assemblyName, _asmName)
+                            && AssemblyHelper.ValidateArchitecture(_asmName)) {
+
+                            files.Add(file);
+                        }
+                    }
+                }
+            }
+
+            return files;
+        }
+
+        #endregion
+
     }
+
 }

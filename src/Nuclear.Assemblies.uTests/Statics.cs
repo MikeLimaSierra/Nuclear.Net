32ffc4f654383de20a9c055d4b1da12f56688134
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-06-16 22:11:07 +0200

    Create Statics.cs

---
+++ src/Nuclear.Assemblies.uTests/Statics.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+using Nuclear.Assemblies.Resolvers;
+using Nuclear.Extensions;
+
+namespace Nuclear.Assemblies {
+    internal static class Statics {
+
+        internal static IEqualityComparer<FileInfo> FileInfoComparer { get; } = DynamicEqualityComparer.FromDelegate<FileInfo>(
+            (x, y) => (x == null && y == null) || (x != null && y != null && x.FullName.Equals(y.FullName)),
+            obj => obj.GetHashCode());
+
+        internal static Assembly TestAsm { get; } = typeof(DefaultResolver_uTests).Assembly;
+
+        internal static FileInfo TestPath { get; } = new FileInfo(TestAsm.Location);
+
+        internal static DirectoryInfo FakeNugetCache { get; }
+
+        static Statics() {
+            DirectoryInfo repodir = TestPath.Directory.Parent.Parent.Parent.Parent.Parent;
+
+            FakeNugetCache = new DirectoryInfo(Path.Combine(repodir.FullName, "fake-nuget", "packages"));
+        }
+
+    }
+}

2098e74449be9c7f649da6a14e547c84523969ed
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-06-17 19:24:23 +0200

    Create ISemVer.cs

---
+++ src/Nuclear.SemVer/ISemVer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Nuclear.SemVer {
+
+    /// <summary>
+    /// Defines a semantic version object.
+    /// </summary>
+    public interface ISemVer {
+
+        #region properties
+
+        /// <summary>
+        /// Gets the major part of the version.
+        /// </summary>
+        Int32 Major { get; }
+
+        /// <summary>
+        /// Gets the minor part of the version.
+        /// </summary>
+        Int32 Minor { get; }
+
+        /// <summary>
+        /// Gets the patch part of the version.
+        /// </summary>
+        Int32 Patch { get; }
+
+        #endregion
+
+    }
+}

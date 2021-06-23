a9404484ddef28cf2523b436432f5a9eb2239dc3
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-06-23 13:03:01 +0200

    Create SemVer.cs

---
+++ src/Nuclear.SemVer/SemVer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nuclear.SemVer {
+    internal class SemVer : ISemVer {
+
+        #region properties
+        
+        public Int32 Major { get; }
+        
+        public Int32 Minor { get; }
+        
+        public Int32 Patch { get; }
+        
+        public Boolean IsPreRelease { get; }
+        
+        public String PreRelease { get; }
+        
+        public Boolean HasMetaData { get; }
+        
+        public String MetaData { get; }
+
+        #endregion
+
+        #region ctors
+
+        public SemVer(String input) {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region methods
+
+        public override String ToString() => base.ToString();
+
+        public override Int32 GetHashCode() => base.GetHashCode();
+
+        public override Boolean Equals(Object obj) => base.Equals(obj);
+
+        #endregion
+
+        #region IEquatable
+
+        public Boolean Equals(ISemVer other) => throw new NotImplementedException();
+
+        #endregion
+
+        #region IComparable
+
+        public Int32 CompareTo(ISemVer other) => throw new NotImplementedException();
+
+        #endregion
+
+    }
+}

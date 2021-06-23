c94b4e1e9675b6ee20f1ad69e1e147bceab3e10b
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-06-23 12:17:21 +0200

    Create ISemVerFactory.cs

---
+++ src/Nuclear.SemVer/Factories/ISemVerFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+using Nuclear.Creation;
+
+namespace Nuclear.SemVer.Factories {
+
+    /// <summary>
+    /// Defines a factory to create semantic versioning objects.
+    /// </summary>
+    public interface ISemVerFactory : ICreator<ISemVer, String> { }
+}

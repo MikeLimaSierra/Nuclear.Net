0953a40dc4db29e3153d1085930db63ba3106356
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-07-01 21:23:34 +0200

    Create ISemVerFactory_uTests.cs

---
+++ src/Nuclear.SemVer.uTests/Factories/ISemVerFactory_uTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Nuclear.Creation;
+using Nuclear.TestSite;
+
+namespace Nuclear.SemVer.Factories {
+    class ISemVerFactory_uTests {
+
+        [TestMethod]
+        void Implementation() {
+
+            Test.If.Type.Implements<ISemVerFactory, ICreator<ISemVer, String>>();
+
+        }
+
+    }
+}

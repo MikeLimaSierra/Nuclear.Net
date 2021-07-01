fcb071e1693db4f452f5a4d5edd2894545a1da50
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-07-01 21:50:34 +0200

    add factory extension for semver

---
+++ src/Nuclear.SemVer/Factories/IFactoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Nuclear.Creation;
+
+namespace Nuclear.SemVer.Factories {
+
+    /// <summary>
+    /// Extends the functionality of <see cref="IFactory"/>.
+    /// </summary>
+    public static class IFactoryExtensions {
+
+        /// <summary>
+        /// Returns a new instance of type <see cref="ISemVerFactory"/>.
+        /// </summary>
+        /// <param name="this">The extended <see cref="IFactory"/> instance.</param>
+        /// <returns>A new instance of type <see cref="ISemVerFactory"/>.</returns>
+#pragma warning disable IDE0060 // Remove unused parameter
+        public static ISemVerFactory SemVer(this IFactory @this) => new SemVerFactory();
+#pragma warning restore IDE0060 // Remove unused parameter
+
+    }
+
+}

0b2a7cc20b1f8ab1d144feb4b78f15d52ab0dd8e
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-10-27 23:22:09 +0200

    Update SemVer_uTests.cs

--- src/Nuclear.SemVer.uTests/SemVer_uTests.cs
+++ src/Nuclear.SemVer.uTests/SemVer_uTests.cs
@@ -100,7 +100,101 @@ namespace Nuclear.SemVer {
         #region ctors
 
         [TestMethod]
-        void CtorThrows() {
+        [TestParameters(0u, 0u, 0u)]
+        [TestParameters(1u, 2u, 3u)]
+        [TestParameters(3u, 2u, 1u)]
+        void Ctor(UInt32 major, UInt32 minor, UInt32 patch) {
+
+            SemanticVersion sut = default;
+
+            Test.IfNot.Action.ThrowsException(() => sut = new SemanticVersion(major, minor, patch), out Exception _);
+
+            Test.If.Value.IsEqual(sut.Major, major);
+            Test.If.Value.IsEqual(sut.Minor, minor);
+            Test.If.Value.IsEqual(sut.Patch, patch);
+            Test.If.Object.IsNull(sut.PreRelease);
+            Test.If.Value.IsFalse(sut.IsPreRelease);
+            Test.If.Object.IsNull(sut.MetaData);
+            Test.If.Value.IsFalse(sut.HasMetaData);
+
+        }
+
+        [TestMethod]
+        [TestParameters(0u, 0u, 0u, "alpha")]
+        [TestParameters(1u, 2u, 3u, "alpha-alpha")]
+        [TestParameters(3u, 2u, 1u, "4.5.6")]
+        void Ctor_WithPreRelease(UInt32 major, UInt32 minor, UInt32 patch, String preRelease) {
+
+            SemanticVersion sut = default;
+
+            Test.IfNot.Action.ThrowsException(() => sut = new SemanticVersion(major, minor, patch, preRelease: preRelease), out Exception _);
+
+            Test.If.Value.IsEqual(sut.Major, major);
+            Test.If.Value.IsEqual(sut.Minor, minor);
+            Test.If.Value.IsEqual(sut.Patch, patch);
+            Test.If.Value.IsEqual(sut.PreRelease, preRelease);
+            Test.If.Value.IsTrue(sut.IsPreRelease);
+            Test.If.Object.IsNull(sut.MetaData);
+            Test.If.Value.IsFalse(sut.HasMetaData);
+
+        }
+
+        #endregion
+
+        #region ValidatePreRelease
+
+        [TestMethod]
+        [TestParameters(null, false)]
+        [TestParameters("", false)]
+        [TestParameters(" ", false)]
+        [TestParameters("0", true)]
+        [TestParameters("01", false)]
+        [TestParameters("1.02", false)]
+        [TestParameters("1.2", true)]
+        [TestParameters("1.2.3", true)]
+        [TestParameters("a.2.c.45", true)]
+        [TestParameters("alpha", true)]
+        [TestParameters("alpha-alpha", true)]
+        [TestParameters("alpha.1", true)]
+        [TestParameters("abc/def", false)]
+        [TestParameters("abc_def", false)]
+        void ValidatePreRelease(String input, Boolean expected) {
+
+            Boolean result = default;
+
+            Test.IfNot.Action.ThrowsException(() => result = SemanticVersion.ValidatePreRelease(input), out Exception _);
+
+            Test.If.Value.IsEqual(result, expected);
+
+        }
+
+        #endregion
+
+        #region ValidateMetaData
+
+        [TestMethod]
+        [TestParameters(null, false)]
+        [TestParameters("", false)]
+        [TestParameters(" ", false)]
+        [TestParameters("0", true)]
+        [TestParameters("01", true)]
+        [TestParameters("1.02", true)]
+        [TestParameters("1.2", true)]
+        [TestParameters("1.2.3", true)]
+        [TestParameters("a.2.c.45", true)]
+        [TestParameters("alpha", true)]
+        [TestParameters("alpha-alpha", true)]
+        [TestParameters("alpha.1", true)]
+        [TestParameters("abc/def", false)]
+        [TestParameters("abc_def", false)]
+        void ValidateMetaData(String input, Boolean expected) {
+
+            Boolean result = default;
+
+            Test.IfNot.Action.ThrowsException(() => result = SemanticVersion.ValidateMetaData(input), out Exception _);
+
+            Test.If.Value.IsEqual(result, expected);
+
         }
 
         #endregion

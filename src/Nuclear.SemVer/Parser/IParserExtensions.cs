5b74d9a2d509215e78e9b6d38c201aff83a8b0d2
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-10-29 16:23:28 +0200

    asdsf

---
+++ src/Nuclear.SemVer/Parser/IParserExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+using Nuclear.Creation;
+
+namespace Nuclear.SemVer.Parser {
+
+    /// <summary>
+    /// Defines an extension to <see cref="IParser"/>.
+    /// </summary>
+    public static class IParserExtensions {
+
+        /// <summary>
+        /// Returns a new instance of <see cref="ISemVerParser"/>.
+        /// </summary>
+        /// <returns>A new instance of <see cref="ISemVerParser"/>.</returns>
+        public static ISemVerParser SemVer(this IParser _) => new SemVerParser();
+
+    }
+}
---
+++ src/Nuclear.SemVer/Parser/ISemVerParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using Nuclear.Creation;
+
+namespace Nuclear.SemVer.Parser {
+
+    /// <summary>
+    /// Defines a parser to create instances of <see cref="SemanticVersion"/> from a <see cref="String"/>.
+    /// </summary>
+    public interface ISemVerParser : ICreator<SemanticVersion, String> { }
+
+}

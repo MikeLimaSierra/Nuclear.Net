ea41a892a6c08964e9432a780ea66e922057b7f6
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-11-02 21:16:16 +0100

    Update IFactory.cs

--- src/Nuclear.Creation/IFactory.cs
+++ src/Nuclear.Creation/IFactory.cs
@@ -3,6 +3,17 @@
     /// <summary>
     /// Defines a most basic factory structure used for extensibility.
     /// </summary>
-    public interface IFactory { }
+    public interface IFactory {
+
+        #region properties
+
+        /// <summary>
+        /// Gets a new factory instance for creator objects.
+        /// </summary>
+        ICreatorFactory Creator { get; }
+
+        #endregion
+
+    }
 
 }

53a56965fc226265f1f7ab61b56bcd7fd5846520
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-11-02 21:16:57 +0100

    Update InternalFactory_uTests.cs

--- src/Nuclear.Creation.uTests/InternalFactory_uTests.cs
+++ src/Nuclear.Creation.uTests/InternalFactory_uTests.cs
@@ -1,4 +1,6 @@
-﻿using Nuclear.TestSite;
+﻿using System;
+
+using Nuclear.TestSite;
 
 namespace Nuclear.Creation {
     class InternalFactory_uTests {
@@ -7,6 +9,21 @@ namespace Nuclear.Creation {
         void Implementation() {
 
             Test.If.Type.Implements<InternalFactory, IFactory>();
+
+        }
+
+        [TestMethod]
+        void Creator() {
+
+            ICreatorFactory fac1 = default;
+            ICreatorFactory fac2 = default;
+
+            Test.IfNot.Action.ThrowsException(() => fac1 = Factory.Instance.Creator, out Exception _);
+            Test.IfNot.Action.ThrowsException(() => fac2 = Factory.Instance.Creator, out Exception _);
+
+            Test.IfNot.Object.IsNull(fac1);
+            Test.IfNot.Object.IsNull(fac2);
+            Test.IfNot.Reference.IsEqual(fac1, fac2);
 
         }
 

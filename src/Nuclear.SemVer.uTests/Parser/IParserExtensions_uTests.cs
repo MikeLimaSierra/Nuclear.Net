0f3800c02198e27e07b96d894cd5a09b161c4a5e
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-11-05 21:24:37 +0100

    Create IParserExtensions_uTests.cs

---
+++ src/Nuclear.SemVer.uTests/Parser/IParserExtensions_uTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Nuclear.Creation;
+using Nuclear.TestSite;
+
+namespace Nuclear.SemVer.Parser {
+    class IParserExtensions_uTests {
+
+        #region SemVer
+
+        [TestMethod]
+        public void MyTestMethod() {
+
+            ICreator<SemanticVersion, String> fac1 = default;
+            ICreator<SemanticVersion, String> fac2 = default;
+
+            Test.IfNot.Action.ThrowsException(() => fac1 = Creation.Parser.Instance.SemVer(), out Exception _);
+            Test.IfNot.Action.ThrowsException(() => fac2 = Creation.Parser.Instance.SemVer(), out Exception _);
+
+            Test.IfNot.Object.IsNull(fac1);
+            Test.IfNot.Object.IsNull(fac2);
+            Test.IfNot.Reference.IsEqual(fac1, fac2);
+
+        }
+
+        #endregion
+
+    }
+}

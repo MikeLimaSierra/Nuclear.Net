1b78becffbdbe6e3767dc22ead0c11ee565f663d
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-11-06 00:04:32 +0100

    update factories for new creation release

--- src/Nuclear.Assemblies/Factories/DefaultResolverFactory.cs
+++ src/Nuclear.Assemblies/Factories/DefaultResolverFactory.cs
@@ -1,17 +1,35 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 using Nuclear.Assemblies.Resolvers;
 using Nuclear.Assemblies.Resolvers.Data;
+using Nuclear.Creation;
 
 namespace Nuclear.Assemblies.Factories {
 
     internal class DefaultResolverFactory : IDefaultResolverFactory {
 
+        #region fields
+
+        private static readonly ICreator<IDefaultResolver> _resolverCreator = Factory.Instance.Creator.Create<IDefaultResolver>(() => new DefaultResolver());
+
+        private static readonly ICreator<IDefaultResolverData, FileInfo> _dataCreator = Factory.Instance.Creator.Create<IDefaultResolverData, FileInfo>((file) => new DefaultResolverData(file));
+
+        #endregion
+
         #region methods
 
-        public void Create(out IDefaultResolver obj) => obj = new DefaultResolver();
+        public void Create(out IDefaultResolver obj) => _resolverCreator.Create(out obj);
 
-        public void Create(out IDefaultResolverData obj, FileInfo file) => obj = new DefaultResolverData(file);
+        public Boolean TryCreate(out IDefaultResolver obj) => _resolverCreator.TryCreate(out obj);
+
+        public Boolean TryCreate(out IDefaultResolver obj, out Exception ex) => _resolverCreator.TryCreate(out obj, out ex);
+
+        public void Create(out IDefaultResolverData obj, FileInfo file) => _dataCreator.Create(out obj, file);
+
+        public Boolean TryCreate(out IDefaultResolverData obj, FileInfo in1) => _dataCreator.TryCreate(out obj, in1);
+
+        public Boolean TryCreate(out IDefaultResolverData obj, FileInfo in1, out Exception ex) => _dataCreator.TryCreate(out obj, in1, out ex);
 
         #endregion
 
--- src/Nuclear.Assemblies/Factories/NugetResolverFactory.cs
+++ src/Nuclear.Assemblies/Factories/NugetResolverFactory.cs
@@ -1,17 +1,35 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 using Nuclear.Assemblies.Resolvers;
 using Nuclear.Assemblies.Resolvers.Data;
+using Nuclear.Creation;
 
 namespace Nuclear.Assemblies.Factories {
 
     internal class NugetResolverFactory : INugetResolverFactory {
 
+        #region fields
+
+        private static readonly ICreator<INugetResolver> _resolverCreator = Factory.Instance.Creator.Create<INugetResolver>(() => new NugetResolver());
+
+        private static readonly ICreator<INugetResolverData, FileInfo> _dataCreator = Factory.Instance.Creator.Create<INugetResolverData, FileInfo>((file) => new NugetResolverData(file));
+
+        #endregion
+
         #region methods
 
-        public void Create(out INugetResolver obj) => obj = new NugetResolver();
+        public void Create(out INugetResolver obj) => _resolverCreator.Create(out obj);
 
-        public void Create(out INugetResolverData obj, FileInfo file) => obj = new NugetResolverData(file);
+        public Boolean TryCreate(out INugetResolver obj) => _resolverCreator.TryCreate(out obj);
+
+        public Boolean TryCreate(out INugetResolver obj, out Exception ex) => _resolverCreator.TryCreate(out obj, out ex);
+
+        public void Create(out INugetResolverData obj, FileInfo file) => _dataCreator.Create(out obj, file);
+
+        public Boolean TryCreate(out INugetResolverData obj, FileInfo in1) => _dataCreator.TryCreate(out obj, in1);
+
+        public Boolean TryCreate(out INugetResolverData obj, FileInfo in1, out Exception ex) => _dataCreator.TryCreate(out obj, in1, out ex);
 
         #endregion
 

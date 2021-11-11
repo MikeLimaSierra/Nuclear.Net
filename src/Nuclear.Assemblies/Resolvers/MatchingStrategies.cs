c04050b4683724a19f5d21efbcb7fb81af578a07
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-11-11 20:58:04 +0100

    Create MatchingStrategies.cs

---
+++ src/Nuclear.Assemblies/Resolvers/MatchingStrategies.cs
@@ -0,0 +1,23 @@
+﻿namespace Nuclear.Assemblies.Resolvers {
+
+    /// <summary>
+    /// Defines a range of version matching strategies to resolve assemblies.
+    /// </summary>
+    public enum MatchingStrategies {
+
+        /// <summary>
+        /// Invalid value.
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// Versions must match exactly.
+        /// </summary>
+        Strict = 1,
+
+        /// <summary>
+        /// Version matching is done according to semver.
+        /// </summary>
+        SemVer = 2,
+    }
+}

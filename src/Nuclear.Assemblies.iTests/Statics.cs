c022941e3562974eba862fe3a0525be5d366f7cb
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-11-11 20:56:31 +0100

    Update Statics.cs

--- src/Nuclear.Assemblies.iTests/Statics.cs
+++ src/Nuclear.Assemblies.iTests/Statics.cs
@@ -40,34 +40,34 @@ namespace Nuclear.Assemblies {
         internal static String ComplexFakePackageName => "Awesome.Nuget.Package";
 
         internal static List<String> ComplexFakePackageVersions => new List<String>() {
-                "1.1.0",
-                "1.1.0-beta",
-                "1.1.0-beta+meta",
-                "1.1.1",
-                "1.2.0",
-                "1.3.0",
-                "2.1.0",
-                "2.1.1",
-                "2.2.0",
-                "2.3.0",
-                "3.1.0",
-                "3.1.1",
-                "3.2.0",
-                "3.3.0"
+            "1.1.0",
+            "1.1.0-beta",
+            "1.1.0-beta+meta",
+            "1.1.1",
+            "1.2.0",
+            "1.3.0",
+            "2.1.0",
+            "2.1.1",
+            "2.2.0",
+            "2.3.0",
+            "3.1.0",
+            "3.1.1",
+            "3.2.0",
+            "3.3.0"
         };
 
         internal static List<String> ComplexFakePackageTfms => new List<String>() {
-                "net45",
-                "net46",
-                "net48",
-                "netcoreapp1.0",
-                "netcoreapp2.0",
-                "netcoreapp3.0",
-                "netcoreapp3.1",
-                "net5.0",
-                "netstandard1.0",
-                "netstandard2.0",
-                "netstandard2.1"
+            "net45",
+            "net46",
+            "net48",
+            "netcoreapp1.0",
+            "netcoreapp2.0",
+            "netcoreapp3.0",
+            "netcoreapp3.1",
+            "net5.0",
+            "netstandard1.0",
+            "netstandard2.0",
+            "netstandard2.1"
         };
 
         static Statics() {

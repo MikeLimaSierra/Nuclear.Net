3b93f29630ca5e320ddb6e94ff539eabfb366e97
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-11-11 20:59:12 +0100

    split assemblyresolver

---
+++ src/Nuclear.Assemblies/Resolvers/AssemblyResolver.Generic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Nuclear.Assemblies.Resolvers.Data;
+
+namespace Nuclear.Assemblies.Resolvers {
+
+    internal abstract class AssemblyResolver<TData> : AssemblyResolver, IAssemblyResolver<TData> where TData : IAssemblyResolverData {
+
+        #region public methods
+
+        public abstract Boolean TryResolve(ResolveEventArgs e, out IEnumerable<TData> data);
+
+        public abstract Boolean TryResolve(String fullName, out IEnumerable<TData> data);
+
+        public abstract Boolean TryResolve(AssemblyName assemblyName, out IEnumerable<TData> data);
+
+        #endregion
+
+    }
+
+}
--- src/Nuclear.Assemblies/Resolvers/AssemblyResolver.cs
+++ src/Nuclear.Assemblies/Resolvers/AssemblyResolver.cs
@@ -1,45 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
-
-using Nuclear.Assemblies.Resolvers.Data;
 
 namespace Nuclear.Assemblies.Resolvers {
 
-    /// <summary>
-    /// Abstract base implementation of <see cref="IAssemblyResolver{TData}"/> including some basic resolving functionality.
-    /// </summary>
-    /// <typeparam name="TData">The type of the resolver data, must inherit from <see cref="IAssemblyResolverData"/>.</typeparam>
-    internal abstract class AssemblyResolver<TData> : IAssemblyResolver<TData> where TData : IAssemblyResolverData {
+    internal abstract class AssemblyResolver : IAssemblyResolver {
 
-        #region public methods
+        #region properties
 
-        /// <summary>
-        /// Resolves a reference assembly by <see cref="ResolveEventArgs"/>.
-        /// A return value indicates if the resolving operation was successful.
-        /// </summary>
-        /// <param name="e">The given <see cref="ResolveEventArgs"/>.</param>
-        /// <param name="data">The resolved data.</param>
-        /// <returns>True if a file could be found.</returns>
-        public abstract Boolean TryResolve(ResolveEventArgs e, out IEnumerable<TData> data);
+        public MatchingStrategies MatchingStrategy { get; }
 
-        /// <summary>
-        /// Resolves a reference assembly by the full name.
-        /// A return value indicates if the resolving operation was successful.
-        /// </summary>
-        /// <param name="fullName">The full name of the assembly.</param>
-        /// <param name="data">The resolved data.</param>
-        /// <returns>True if a file could be found.</returns>
-        public abstract Boolean TryResolve(String fullName, out IEnumerable<TData> data);
+        #endregion
 
-        /// <summary>
-        /// Resolves a reference assembly by an <see cref="AssemblyName"/>.
-        /// A return value indicates if the resolving operation was successful.
-        /// </summary>
-        /// <param name="assemblyName">The <see cref="AssemblyName"/> of the assembly.</param>
-        /// <param name="data">The resolved data.</param>
-        /// <returns>True if a file could be found.</returns>
-        public abstract Boolean TryResolve(AssemblyName assemblyName, out IEnumerable<TData> data);
+        #region protected methods
+
+        protected internal static Boolean VersionsMatch(MatchingStrategies strategy, Version requested, Version found) {
+            switch(strategy) {
+                case MatchingStrategies.Strict: return requested.Equals(found);
+
+                default: return false;
+            }
+        }
 
         #endregion
 

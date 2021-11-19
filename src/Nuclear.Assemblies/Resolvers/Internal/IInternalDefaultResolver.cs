d5aa36af018537998c51ac026f3f0cac250be221
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-11-19 21:45:13 +0100

    add internal default resolver

---
+++ src/Nuclear.Assemblies/Resolvers/Internal/IInternalDefaultResolver.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Nuclear.Assemblies.Resolvers.Internal {
+    internal interface IInternalDefaultResolver {
+
+        #region methods
+
+        IEnumerable<FileInfo> Resolve(AssemblyName assemblyName, DirectoryInfo searchDir, SearchOption searchOption, MatchingStrategies strategy);
+
+        #endregion
+
+    }
+}
---
+++ src/Nuclear.Assemblies/Resolvers/Internal/InternalDefaultResolver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+using Nuclear.Assemblies.Extensions;
+
+namespace Nuclear.Assemblies.Resolvers.Internal {
+    internal class InternalDefaultResolver : IInternalDefaultResolver {
+
+        #region methods
+
+        public IEnumerable<FileInfo> Resolve(AssemblyName assemblyName, DirectoryInfo searchDir, SearchOption searchOption, MatchingStrategies strategy) {
+            List<FileInfo> files = new List<FileInfo>();
+
+            if(assemblyName != null && searchDir != null) {
+
+                foreach(String extension in AssemblyHelper.AssemblyFileExtensions) {
+                    foreach(FileInfo file in searchDir.GetFiles($"{assemblyName.Name}.{extension}", searchOption)) {
+
+                        if(AssemblyHelper.TryGetAssemblyName(file, out AssemblyName _asmName)
+                            && assemblyName.Name == _asmName.Name
+                            && assemblyName.Version.Matches(_asmName.Version, strategy)
+                            && AssemblyHelper.ValidateArchitecture(_asmName)) {
+
+                            files.Add(file);
+                        }
+                    }
+                }
+            }
+
+            return files;
+        }
+
+        #endregion
+
+    }
+}

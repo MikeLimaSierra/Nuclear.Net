39c94f17335fa688713c58583a47d37d82aacdac
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-11-19 21:45:45 +0100

    Create VersionExtensions_uTests.cs

---
+++ src/Nuclear.Assemblies.uTests/Extensions/VersionExtensions_uTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Nuclear.Assemblies.Resolvers;
+using Nuclear.TestSite;
+
+namespace Nuclear.Assemblies.Extensions {
+    class VersionExtensions_uTests {
+
+        #region VersionsMatch
+
+        [TestMethod]
+        [TestData(nameof(VersionsMatch_Data))]
+        void VersionsMatch(MatchingStrategies strategy, Version requested, Version found, Boolean expected) {
+
+            Boolean result = default;
+
+            Test.IfNot.Action.ThrowsException(() => result = requested.Matches(found, strategy), out Exception _);
+
+            Test.If.Value.IsEqual(result, expected);
+
+        }
+
+        IEnumerable<Object[]> VersionsMatch_Data() {
+            return new List<Object[]>() {
+                new Object[] { MatchingStrategies.Unknown, new Version(1, 2, 2), new Version(2, 2, 2), false },
+                new Object[] { MatchingStrategies.Unknown, new Version(2, 1, 2), new Version(2, 2, 2), false },
+                new Object[] { MatchingStrategies.Unknown, new Version(2, 2, 1), new Version(2, 2, 2), false },
+                new Object[] { MatchingStrategies.Unknown, new Version(2, 2, 2), new Version(2, 2, 2), false },
+                new Object[] { MatchingStrategies.Unknown, new Version(2, 2, 2), new Version(1, 2, 2), false },
+                new Object[] { MatchingStrategies.Unknown, new Version(2, 2, 2), new Version(2, 1, 2), false },
+                new Object[] { MatchingStrategies.Unknown, new Version(2, 2, 2), new Version(2, 2, 1), false },
+
+                new Object[] { MatchingStrategies.Strict, new Version(1, 2, 2), new Version(2, 2, 2), false },
+                new Object[] { MatchingStrategies.Strict, new Version(2, 1, 2), new Version(2, 2, 2), false },
+                new Object[] { MatchingStrategies.Strict, new Version(2, 2, 1), new Version(2, 2, 2), false },
+                new Object[] { MatchingStrategies.Strict, new Version(2, 2, 2), new Version(2, 2, 2), true },
+                new Object[] { MatchingStrategies.Strict, new Version(2, 2, 2), new Version(1, 2, 2), false },
+                new Object[] { MatchingStrategies.Strict, new Version(2, 2, 2), new Version(2, 1, 2), false },
+                new Object[] { MatchingStrategies.Strict, new Version(2, 2, 2), new Version(2, 2, 1), false },
+
+                new Object[] { MatchingStrategies.SemVer, new Version(1, 2, 2), new Version(2, 2, 2), false },
+                new Object[] { MatchingStrategies.SemVer, new Version(2, 1, 2), new Version(2, 2, 2), true },
+                new Object[] { MatchingStrategies.SemVer, new Version(2, 2, 1), new Version(2, 2, 2), true },
+                new Object[] { MatchingStrategies.SemVer, new Version(2, 2, 2), new Version(2, 2, 2), true },
+                new Object[] { MatchingStrategies.SemVer, new Version(2, 2, 2), new Version(1, 2, 2), false },
+                new Object[] { MatchingStrategies.SemVer, new Version(2, 2, 2), new Version(2, 1, 2), false },
+                new Object[] { MatchingStrategies.SemVer, new Version(2, 2, 2), new Version(2, 2, 1), true },
+            };
+        }
+
+        #endregion
+
+    }
+}

ec27906011758f05fc206dc9b6592a4247808fca
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-11-19 21:45:24 +0100

    Create VersionExtensions.cs

---
+++ src/Nuclear.Assemblies/Extensions/VersionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Nuclear.Assemblies.Resolvers;
+
+namespace Nuclear.Assemblies.Extensions {
+    internal static class VersionExtensions {
+
+        internal static Boolean Matches(this Version requested, Version found, MatchingStrategies strategy)
+            => strategy switch {
+                MatchingStrategies.Strict => requested.Equals(found),
+                MatchingStrategies.SemVer => requested.Major == found.Major && requested.Minor <= found.Minor,
+                _ => false,
+            };
+
+    }
+}

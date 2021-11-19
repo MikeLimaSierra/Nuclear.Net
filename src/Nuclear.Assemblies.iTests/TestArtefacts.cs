ed8682e710e646ece27d924277337ded49c6f6fa
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-11-19 21:54:13 +0100

    Update TestArtefacts.cs

--- src/Nuclear.Assemblies.iTests/TestArtefacts.cs
+++ src/Nuclear.Assemblies.iTests/TestArtefacts.cs
@@ -31,38 +31,9 @@ namespace Nuclear.Assemblies {
 
         IEnumerable<Object[]> ArtefactExists_ComplexPackage_Data() {
             var packageRoot = Path.Combine(Statics.FakeNugetCache.FullName, Statics.ComplexFakePackageName);
-            var versions = new List<String>() {
-                "1.1.0",
-                "1.1.0-beta",
-                "1.1.0-beta+meta",
-                "1.1.1",
-                "1.2.0",
-                "1.3.0",
-                "2.1.0",
-                "2.1.1",
-                "2.2.0",
-                "2.3.0",
-                "3.1.0",
-                "3.1.1",
-                "3.2.0",
-                "3.3.0"
-            };
-            var tfms = new List<String>() {
-                "net45",
-                "net46",
-                "net48",
-                "netcoreapp1.0",
-                "netcoreapp2.0",
-                "netcoreapp3.0",
-                "netcoreapp3.1",
-                "net5.0",
-                "netstandard1.0",
-                "netstandard2.0",
-                "netstandard2.1"
-            };
 
-            foreach(var version in versions) {
-                foreach(var tfm in tfms) {
+            foreach(var version in Statics.ComplexFakePackageVersions) {
+                foreach(var tfm in Statics.ComplexFakePackageTfms) {
                     yield return new Object[] { Path.Combine(packageRoot, version, "lib", tfm, $"{Statics.ComplexFakePackageName}.dll") };
                     yield return new Object[] { Path.Combine(packageRoot, version, "lib", "x86", tfm, $"{Statics.ComplexFakePackageName}.dll") };
                     yield return new Object[] { Path.Combine(packageRoot, version, "lib", "x64", tfm, $"{Statics.ComplexFakePackageName}.dll") };

02433a3e5c6dfe2ac962047826b55fcfec21903e
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-11-25 22:14:43 +0100

    Update VersionExtensions.cs

--- src/Nuclear.Assemblies/Extensions/VersionExtensions.cs
+++ src/Nuclear.Assemblies/Extensions/VersionExtensions.cs
@@ -5,12 +5,16 @@ using Nuclear.Assemblies.Resolvers;
 namespace Nuclear.Assemblies.Extensions {
     internal static class VersionExtensions {
 
-        internal static Boolean Matches(this Version requested, Version found, VersionMatchingStrategies strategy)
-            => strategy switch {
+        internal static Boolean Matches(this Version requested, Version found, VersionMatchingStrategies strategy) {
+            if(requested == null || found == null) {
+                return false;
+            }
+
+            return strategy switch {
                 VersionMatchingStrategies.Strict => requested.Major == found.Major && requested.Minor == found.Minor && requested.Build == found.Build,
                 VersionMatchingStrategies.SemVer => requested.Major == found.Major && requested.Minor <= found.Minor,
                 _ => false,
             };
-
+        }
     }
 }

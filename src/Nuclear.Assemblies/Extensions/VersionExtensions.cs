148caf3286352056e757caa7675ccbc5d6cad33b
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-12-03 18:23:10 +0100

    Update VersionExtensions.cs

--- src/Nuclear.Assemblies/Extensions/VersionExtensions.cs
+++ src/Nuclear.Assemblies/Extensions/VersionExtensions.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using Nuclear.Assemblies.Resolvers;
+using Nuclear.Extensions;
 
 namespace Nuclear.Assemblies.Extensions {
     internal static class VersionExtensions {
+
+        private static readonly IEqualityComparer<Version> _strictComparer =
+            DynamicEqualityComparer.FromDelegate<Version>((x, y) => x.Major == y.Major && x.Minor == y.Minor && x.Build == y.Build, (x) => x.GetHashCode());
+
+        private static readonly IEqualityComparer<Version> _semVerComparer =
+            DynamicEqualityComparer.FromDelegate<Version>((x, y) => x.Major == y.Major && x.Minor <= y.Minor, (x) => x.GetHashCode());
 
         internal static Boolean Matches(this Version requested, Version found, VersionMatchingStrategies strategy) {
             if(requested == null || found == null) {
@@ -11,8 +19,8 @@ namespace Nuclear.Assemblies.Extensions {
             }
 
             return strategy switch {
-                VersionMatchingStrategies.Strict => requested.Major == found.Major && requested.Minor == found.Minor && requested.Build == found.Build,
-                VersionMatchingStrategies.SemVer => requested.Major == found.Major && requested.Minor <= found.Minor,
+                VersionMatchingStrategies.Strict => _strictComparer.Equals(requested, found),
+                VersionMatchingStrategies.SemVer => _semVerComparer.Equals(requested, found),
                 _ => false,
             };
         }
